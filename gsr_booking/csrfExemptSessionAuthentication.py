f43d69ef86da1c8a6ff423834b7176dd6d554ed5
Davis Haupt <davis.haupt@gmail.com>
2020-01-26 18:02:45 -0500

    fix lint 2 the squeakwel

--- gsr_booking/csrfExemptSessionAuthentication.py
+++ gsr_booking/csrfExemptSessionAuthentication.py
@@ -2,6 +2,5 @@ from rest_framework.authentication import SessionAuthentication
 
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
-
     def enforce_csrf(self, request):
         return  # To not perform the csrf check previously happening
