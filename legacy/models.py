b8ae356585dfa43c761b055cf5585faf6cfd13bb
Davis Haupt <davis.haupt@gmail.com>
2020-02-09 19:37:28 -0500

    fix more lint

--- legacy/models.py
+++ legacy/models.py
@@ -3,7 +3,8 @@
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and
+#   * delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
