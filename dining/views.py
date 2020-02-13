44b1b198b4c9796e674dde1875dab8cec7ca5f89
Adam Strike <astrike30@users.noreply.github.com>
2020-02-12 23:06:07 -0500

    Added Dashboard view

--- dining/views.py
+++ dining/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import JsonResponse
+from datetime import timedelta
+
+from legacy.models import Account, DiningBalance, DiningTransaction, Course
+
 
 # Create your views here.
+
+class Dashboard(APIView):
+
+	def get(self, request, format=None):
+		return JsonResponse({})
