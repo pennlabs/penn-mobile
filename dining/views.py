cf488a5117613e2fc1198a0c6e523f240afad997
Adam Strike <astrike30@users.noreply.github.com>
2020-02-12 23:07:28 -0500

    Added balance and recent transactions

--- dining/views.py
+++ dining/views.py
@@ -11,4 +11,44 @@ from legacy.models import Account, DiningBalance, DiningTransaction, Course
 class Dashboard(APIView):
 
 	def get(self, request, format=None):
-		return JsonResponse({})
+		self.pennkey = request.GET.get("pennid")
+		self.uid = Account.objects.filter(pennkey=self.pennkey)[0].id
+		json = self.balance()
+
+		json["cards"] = []
+		json["cards"].append(self.recent_transactions_card())
+
+		return JsonResponse(json)
+
+	def balance(self):
+		balance = DiningBalance.objects.filter(account=self.uid).order_by("-created_at")
+		latest = balance[0]
+		return {
+			"swipes": latest.swipes,
+			"dining-dollars": latest.dining_dollars,
+			"guest-swipes": latest.guest_swipes
+		}
+
+
+	def recent_transactions_card(self):
+
+		card = {
+			"type": "recent-transactions",
+			"header-title": "Transactions",
+			"header-body": "Your recent dining dollar transactions",
+			"data": []
+		}
+
+		transactions = DiningTransaction.objects.filter(account=self.uid).order_by("-date")
+
+		transactions = transactions[0:5]
+
+		for transaction in transactions:
+			card["data"].append({
+					"location": transaction.description,
+					"date": transaction.date.strftime('%Y-%m-%dT%H:%M:%S'),
+					"balance": transaction.balance,
+					"amount": transaction.amount
+			})
+
+		return card
