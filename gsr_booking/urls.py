4c694930eede211f1a0bb29da52f4f6ac6425439
Rehaan <5091484+r3khaan@users.noreply.github.com>
2020-03-13 16:05:06 -0700

    Group Booking Logic (Libcal) (#15)
    * Added book_room route
    
    * Add is_admin method to Group model
    
    * Fixed is_admin bug in group model
    
    * Added functionality to book-room route (now calls labs-api-server
    
    * Added libcal reservations functionality for single-gsr-booking, added param=null checks for appropriate error messages
    
    * Added comments for group-booking logic
    
    * Retrieve members from group in book-room
    
    * book-room now WORKS for libcal :DDD
    
    * Made bookings work for libcal (now will retry if initially failed)
    
    * add booking request serializer
    
    * Refactored lots of code, returning JSON with adequate data now
    
    * Randomizes order, puts requester first, show in 30 minute slots
    
    * Refactored; Now returns failed bookings; Added appropriate error messages
    
    * Moved booking logic from views.py to new file
    
    * Fixed formatting using black -l
    
    * Moved functions from class to top-level and imported individually
    
    * Moved dates from custom date str to isoformat; other minor changes
    
    * Formatted using linter
    
    * Changed docstrings and updated error handling
    
    * Changed name of func; and import names
    
    * Add error message for insufficient credit
    
    * removed unused imports
    
    * fixed linting errors
    
    * Tried ... to add a test
    
    * Fixed linting
    
    * Added documentation for book-room
    
    Co-authored-by: Young Lee <jaeyounglee327@gmail.com>
    Co-authored-by: Davis Haupt <davis.haupt@gmail.com>
    Co-authored-by: Rehaan <rehaanirfan@gmail.com>

--- gsr_booking/urls.py
+++ gsr_booking/urls.py
@@ -7,5 +7,4 @@ router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
 router.register(r"membership", GroupMembershipViewSet)
 router.register(r"groups", GroupViewSet)
-
 urlpatterns = router.urls
