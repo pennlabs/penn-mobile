d835fd6847331a76d0e8f598d3ea7fbe32c3a09d
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-06-23 00:39:05 -0400

    Remove mysql config

--- studentlife/settings/production.py
+++ studentlife/settings/production.py
@@ -4,13 +4,10 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 from studentlife.settings.base import *  # noqa: F401, F403
-from studentlife.settings.base import DATABASES, DOMAIN
+from studentlife.settings.base import DOMAIN
 
 
 DEBUG = True
-
-# Fix MySQL Emoji support
-DATABASES["default"]["OPTIONS"] = {"charset": "utf8mb4"}
 
 # Honour the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
