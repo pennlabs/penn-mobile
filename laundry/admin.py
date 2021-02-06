ffa8e3c23380ffa7b82762acedd7d73f62f509b2
justin zhang <judtin@seas.upenn.edu>
2021-02-05 19:02:15 -0500

    slight modification on usage function, added laundry room model

--- laundry/admin.py
+++ laundry/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from laundry.models import LaundrySnapshot
+from laundry.models import LaundrySnapshot, LaundryRoom
 
 
 admin.site.register(LaundrySnapshot)
+admin.site.register(LaundryRoom)
