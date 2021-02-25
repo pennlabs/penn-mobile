984123a28efedc5ac11da4ca3f5ff4c13b01532b
justin zhang <judtin@seas.upenn.edu>
2021-02-25 14:25:40 -0500

    removed majority of tests to individual testing

--- tests/laundry/test_models.py
+++ tests/laundry/test_models.py
@@ -1,4 +1,3 @@
-from django.core.management import call_command
 from django.test import TestCase
 
 from laundry.models import LaundryRoom, LaundrySnapshot
@@ -6,7 +5,19 @@ from laundry.models import LaundryRoom, LaundrySnapshot
 
 class LaundrySnapshotTestCase(TestCase):
     def setUp(self):
-        call_command("load_laundry_rooms")
+        # populates database with LaundryRooms
+        LaundryRoom.objects.get_or_create(
+            hall_id=0, name="Bishop White", location="Quad", total_washers=9, total_dryers=9
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=1, name="Chestnut Butcher", location="Quad", total_washers=11, total_dryers=11
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=2, name="Class of 1928 Fisher", location="Quad", total_washers=8, total_dryers=8
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=3, name="Craig", location="Quad", total_washers=3, total_dryers=3
+        )
         self.laundry_room = LaundryRoom.objects.get(hall_id=0, name="Bishop White", location="Quad")
         self.snapshot = LaundrySnapshot.objects.create(
             room=self.laundry_room, available_washers=10, available_dryers=10,
@@ -21,7 +32,19 @@ class LaundrySnapshotTestCase(TestCase):
 
 class LaundryRoomTestCase(TestCase):
     def setUp(self):
-        call_command("load_laundry_rooms")
+        # populates database with LaundryRooms
+        LaundryRoom.objects.get_or_create(
+            hall_id=0, name="Bishop White", location="Quad", total_washers=9, total_dryers=9
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=1, name="Chestnut Butcher", location="Quad", total_washers=11, total_dryers=11
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=2, name="Class of 1928 Fisher", location="Quad", total_washers=8, total_dryers=8
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=3, name="Craig", location="Quad", total_washers=3, total_dryers=3
+        )
         self.room = LaundryRoom.objects.create(hall_id=1, name="test hall", location="location")
 
     def test_str(self):
