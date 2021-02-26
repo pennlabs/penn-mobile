f26eefbd187f07e40f120b3e30bdddd27aba2312
justin zhang <judtin@seas.upenn.edu>
2021-02-26 02:20:13 -0500

    added 3 new routes for additional info, swapped usage around to accomodate, added test cases

--- laundry/urls.py
+++ laundry/urls.py
@@ -1,11 +1,24 @@
 from django.urls import path
 
-from laundry.views import HallInfo, Halls, HallUsage, Preferences
+from laundry.views import (
+    HallInfo,
+    Halls,
+    HallUsage,
+    Ids,
+    MultipleHallInfo,
+    Preferences,
+    Status,
+    TwoHalls,
+)
 
 
 urlpatterns = [
     path("halls/", Halls.as_view(), name="halls"),
-    path("halls/<hall_id>/", HallInfo.as_view(), name="hall-info"),
+    path("hall/<hall_id>/", HallInfo.as_view(), name="hall-info"),
     path("usage/<hall_id>/", HallUsage.as_view(), name="hall-usage"),
+    path("rooms/<hall_ids>", MultipleHallInfo.as_view(), name="multiple-hall-info"),
+    path("hall/<hall_id1>/<hall_id2>/", TwoHalls.as_view(), name="two-hall-info"),
+    path("halls/ids/", Ids.as_view(), name="hall-ids"),
+    path("status/", Status.as_view(), name="status"),
     path("preferences/", Preferences.as_view(), name="preferences"),
 ]
