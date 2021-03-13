acc911f82cb47fb61a196edfd722d018d660c613
justin zhang <judtin@seas.upenn.edu>
2021-03-13 17:36:34 -0500

    finished penn sdk migration

--- dining/models.py
+++ dining/models.py
@@ -1,1 +1,29 @@
 # Create your models here.
+
+"""
+class DiningPreference(sqldb.Model):
+    id = sqldb.Column(sqldb.Integer, primary_key=True)
+    created_at = sqldb.Column(sqldb.DateTime, server_default=sqldb.func.now())
+    user_id = sqldb.Column(sqldb.Integer, sqldb.ForeignKey("user.id"), nullable=False)
+    account = sqldb.Column(sqldb.VARCHAR(255), sqldb.ForeignKey("account.id"), nullable=True)
+    venue_id = sqldb.Column(sqldb.Integer, nullable=False)
+
+
+class DiningBalance(sqldb.Model):
+    id = sqldb.Column(sqldb.Integer, primary_key=True)
+    account_id = sqldb.Column(sqldb.VARCHAR(255), sqldb.ForeignKey("account.id"))
+    dining_dollars = sqldb.Column(sqldb.Float, nullable=False)
+    swipes = sqldb.Column(sqldb.Integer, nullable=False)
+    guest_swipes = sqldb.Column(sqldb.Integer, nullable=False)
+    created_at = sqldb.Column(sqldb.DateTime, server_default=sqldb.func.now())
+
+
+class DiningTransaction(sqldb.Model):
+    id = sqldb.Column(sqldb.Integer, primary_key=True)
+    account_id = sqldb.Column(sqldb.VARCHAR(255), sqldb.ForeignKey("account.id"))
+    date = sqldb.Column(sqldb.DateTime, nullable=False)
+    description = sqldb.Column(sqldb.Text, nullable=False)
+    amount = sqldb.Column(sqldb.Float, nullable=False)
+    balance = sqldb.Column(sqldb.Float, nullable=False)
+    created_at = sqldb.Column(sqldb.DateTime, server_default=sqldb.func.now())
+"""
--- dining/urls.py
+++ dining/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from dining.views import Dashboard
+from dining.views import DailyMenu, Dashboard, DiningItem, Hours, Venues, WeeklyMenu
 
 
 app_name = "main"
 
-urlpatterns = [path(r"", Dashboard.as_view())]
+urlpatterns = [
+    path(r"", Dashboard.as_view()),
+    path("venues/", Venues.as_view(), name="venues"),
+    path("hours/<venue_id>/", Hours.as_view(), name="hours"),
+    path("weekly_menu/<venue_id>/", WeeklyMenu.as_view(), name="weekly-menu"),
+    path("daily_menu/<venue_id>/", DailyMenu.as_view(), name="daily-menu"),
+    path("item/<item_id>/", DiningItem.as_view(), name="item-info"),
+]
