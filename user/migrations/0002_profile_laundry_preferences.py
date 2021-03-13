906a5f2109236333846449846474f4428eef4f4e
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-03-13 16:30:45 -0500

    Lint migration

--- user/migrations/0002_profile_laundry_preferences.py
+++ user/migrations/0002_profile_laundry_preferences.py
@@ -6,14 +6,14 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('laundry', '0001_initial'),
-        ('user', '0001_initial'),
+        ("laundry", "0001_initial"),
+        ("user", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='profile',
-            name='laundry_preferences',
-            field=models.ManyToManyField(blank=True, to='laundry.LaundryRoom'),
+            model_name="profile",
+            name="laundry_preferences",
+            field=models.ManyToManyField(blank=True, to="laundry.LaundryRoom"),
         ),
     ]
