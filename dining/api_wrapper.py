d3f6306d09f285f0293e8c0148e17a34e80ff3d1
justin zhang <judtin@seas.upenn.edu>
2021-03-13 22:53:10 -0500

    made new changes

--- dining/api_wrapper.py
+++ dining/api_wrapper.py
@@ -8,6 +8,7 @@ class APIError(ValueError):
 def headers():
     """
     Returns headers necessary for Penn Dining API access
+    TODO: add the username + password to environment
     """
 
     return {
