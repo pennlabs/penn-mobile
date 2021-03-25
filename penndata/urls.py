0b9030b1d8e52e14054086bda3ebf5c253e70fa6
justin zhang <judtin@seas.upenn.edu>
2021-03-24 20:09:09 -0400

    finished homepage, news, and calendar views + tests

--- penndata/urls.py
+++ penndata/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from penndata.views import News
+from penndata.views import Calendar, HomePage, News
 
 
 urlpatterns = [
     path("news/", News.as_view(), name="news"),
+    path("calendar/", Calendar.as_view(), name="calendar"),
+    path("homepage", HomePage.as_view(), name="homepage"),
 ]
