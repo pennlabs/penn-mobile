c38cdcc77e140072825c2bc52959a724229a223b
justin zhang <judtin@seas.upenn.edu>
2021-07-01 19:30:33 -0400

    finished notifications

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import include, path
 
-from user.views import NotificationSettingsView, RegisterNotificationToken, UserView
+from user.views import NotificationSettingsView, RegisterNotificationToken, UserView, SendNotification
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+router.register(r"notifications", SendNotification)
 
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("me/", UserView.as_view(), name="user"),
     path("notifications/settings/", NotificationSettingsView.as_view()),
     path("notifications/register/", RegisterNotificationToken.as_view()),
--- user/views.py
+++ user/views.py
@@ -1,9 +1,22 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from apns2.client import APNsClient
+from apns2.credentials import TokenCredentials
+from apns2.payload import Payload
+
+import os
+
 
 from user.models import NotificationSetting, NotificationToken
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, NotificationTokenSerializer
+
+User = get_user_model()
 
 
 class UserView(generics.RetrieveUpdateAPIView):
@@ -93,3 +106,53 @@ class RegisterNotificationToken(APIView):
                     dev=dev,
                 )
         return Response({"detail": "success"})
+
+
+class SendNotification(viewsets.ViewSet):
+
+    queryset = NotificationToken.objects.all()
+    serializer_class = NotificationTokenSerializer
+
+    @action(detail=False, methods=["post"])
+    def send(self, request):
+        obj = get_object_or_404(NotificationToken, user=request.user, kind=NotificationToken.KIND_IOS)
+        self.send_push_notification(obj.token, request.data['title'], request.data['body'], obj.dev)
+        return Response({"detail": "success"})
+
+    @action(detail=False, methods=["post"])
+    def send_internal(self, request):
+        user = get_object_or_404(User, username=request.user.username)
+        obj = get_object_or_404(NotificationToken, user=user, kind=NotificationToken.KIND_IOS)
+        self.send_push_notification(obj.token, request.data['title'], request.data['body'], obj.dev)
+        return Response({"detail": "success"})
+    
+    @action(detail=False, methods=["post"])
+    def send_token_internal(self, request):
+        self.send_push_notification(request.data['token'], request.data['title'], request.data['body'], True)
+        return Response({"detail": "success"})
+
+    def get_client(self, isDev):
+        auth_key_path = os.path.join(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "ios_key.p8"
+        )
+        auth_key_id = "443RV92X4F"
+        team_id = "VU59R57FGM"
+        token_credentials = TokenCredentials(
+            auth_key_path=auth_key_path, auth_key_id=auth_key_id, team_id=team_id
+        )
+        client = APNsClient(credentials=token_credentials, use_sandbox=isDev)
+        return client
+
+    def send_push_notification(self, token, title, body, isDev=False):
+        client = self.get_client(isDev)
+        alert = {"title": title, "body": body}
+        payload = Payload(alert=alert, sound="default", badge=0)
+        topic = "org.pennlabs.PennMobile"
+        client.send_notification(token, payload, topic)
+
+
+    def send_push_notification_batch(self, notifications, isDev=False):
+        client = self.get_client(isDev)
+        topic = "org.pennlabs.PennMobile"
+        client.send_notification_batch(notifications=notifications, topic=topic)
+
