08c0a1de597f734b41d5b94a4f9e1eecc0ef0f8d
AnnyP <annipan@seas.upenn.edu>
2021-10-07 23:04:01 -0400

    Create model and serializer for Post and PostAccount

--- portal/models.py
+++ portal/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
+import datetime
 
 
 User = get_user_model()
@@ -34,3 +35,33 @@ class PollVote(models.Model):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     poll_options = models.ManyToManyField(PollOption)
     created_date = models.DateTimeField(default=timezone.now)
+
+
+class PostAccount(models.Model):
+    name = models.CharField(null=True)
+    email = models.CharField(max_length=255, unique=True)
+    encrypted_password = models.CharField(max_length=255)
+    reset_password_token = models.CharField(max_length=255, null=True, unique=True)
+    reset_password_token_sent_at = models.DateTimeField(null=True)
+    sign_in_count = models.IntegerField(default=1)
+    last_sign_in_at = models.DateTimeField(default=datetime.now)
+    created_at = models.DateTimeField(default=datetime.now)
+    updated_at = models.DateTimeField(default=datetime.now)
+
+
+class Post(models.Model):
+    account = models.ForeignKey(PostAccount, on_delete=models.CASCADE)
+    source = models.CharField(null=True)
+    title = models.CharField(null=True)
+    subtitle = models.CharField(null=True)
+    time_label = models.CharField(null=True)
+    post_url = models.CharField(null=True)
+    image_url = models.CharField()
+    image_url_cropped = models.CharField()
+    filters = models.BooleanField(default=False)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(nullable=False)
+    approved = models.BooleanField(default=False)
+    testers = models.BooleanField(default=False)
+    emails = models.BooleanField(default=False)
+    created_at = models.DateTimeField()
