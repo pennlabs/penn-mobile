eb991ac6ba63a12177d60ef27f419982457bac82
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-10-10 13:41:39 -0400

    Additional URLs (#49)

--- studentlife/settings/base.py
+++ studentlife/settings/base.py
@@ -12,7 +12,7 @@ import os
 import dj_database_url
 
 
-DOMAIN = os.environ.get("DOMAIN", "example.com")
+DOMAINS = os.environ.get("DOMAINS", "example.com")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
