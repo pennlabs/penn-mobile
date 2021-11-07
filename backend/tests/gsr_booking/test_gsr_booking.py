e1e9cc0ab087b2611e0dd7a1062a67f99d334673
Justin Zhang <judtin@seas.upenn.edu>
2021-11-07 12:59:28 -0500

    Group GSR Cleanup (#48)
    * cleaned up group gsr codebase
    
    * lint
    
    * cleaned up group gsr codebase
    
    Co-authored-by: Armaan Tobaccowalla <armaan@tobaccowalla.com>

--- backend/tests/gsr_booking/test_gsr_booking.py
+++ backend/tests/gsr_booking/test_gsr_booking.py
@@ -71,52 +71,6 @@ class UserViewTestCase(TestCase):
         self.assertEqual(user4, mem.user)
         self.assertEqual("user4", mem.username)
 
-    def test_search_users_first_name(self):
-        response = self.client.get("/users/search/", {"q": "user"})
-        self.assertEqual(200, response.status_code)
-        self.assertEqual(2, len(response.data))
-
-    def test_search_users_full_name(self):
-        response = self.client.get("/users/search/", {"q": "user one"})
-        self.assertEqual(200, response.status_code)
-        self.assertEqual(1, len(response.data))
-
-    def test_search_users_pennkey(self):
-        response = self.client.get("/users/search/", {"q": "user1"})
-        self.assertEqual(200, response.status_code)
-        self.assertEqual(1, len(response.data))
-
-
-class GSRCredentialsViewTestCase(TestCase):
-    def setUp(self):
-        self.user1 = User.objects.create_user(
-            username="user1", password="password", first_name="user", last_name="one"
-        )
-        self.user2 = User.objects.create_user(
-            username="user2", password="password2", first_name="user", last_name="two"
-        )
-        UserSearchIndex.objects.create(user=self.user1)
-        UserSearchIndex.objects.create(user=self.user2)
-
-        self.client = APIClient()
-        self.client.login(username="user1", password="password")
-
-    def test_credentials_create(self):
-        response = self.client.get("/credentials/")
-        self.assertEqual(404, response.status_code)
-        params = {"user": "user1", "session_id": "sid"}
-        response = self.client.put("/credentials/", params, format="json")
-        self.assertEqual(200, response.status_code)
-        self.assertEqual(4, len(response.data))
-        response = self.client.get("/credentials/")
-        self.assertEqual(200, response.status_code)
-        self.assertEqual(4, len(response.data))
-
-    def test_credentials_forbidden(self):
-        params = {"user": "user2", "session_id": "sid2"}
-        response = self.client.put("/credentials/", params, format="json")
-        self.assertEqual(403, response.status_code)
-
 
 class MembershipViewTestCase(TestCase):
     def setUp(self):
