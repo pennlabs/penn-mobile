4b77a81de8b683efbaba6c1320e13a9afbfc9fb8
Justin Zhang <judtin@seas.upenn.edu>
2022-01-20 21:40:58 -0500

    create homepage ordering

--- backend/penndata/admin.py
+++ backend/penndata/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from penndata.models import Event
+from penndata.models import Event, HomePageOrder
 
 
 admin.site.register(Event)
+admin.site.register(HomePageOrder)
