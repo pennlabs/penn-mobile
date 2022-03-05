488bb16163bd0cfadc16a2634ce4a506ea9803d3
Tuneer Roy <tuneer.roy12@gmail.com>
2022-03-05 01:40:17 -0500

    Added notif alert request

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework import routers
 
-from user.views import NotificationView, UserView
+from user.views import NotificationView, UserView, NotificationAlertView
 
 
 app_name = "user"
@@ -11,6 +11,7 @@ router.register(r"notifications", NotificationView, basename="notifications")
 
 additional_urls = [
     path("me/", UserView.as_view(), name="user"),
+    path("alerts/", NotificationAlertView.as_view(), name="alert"),
 ]
 
 urlpatterns = router.urls + additional_urls
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,8 +1,12 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from user.models import NotificationToken
 from user.serializers import NotificationTokenSerializer, UserSerializer
+from user.notifications import send_push_notification
 
 
 class UserView(generics.RetrieveUpdateAPIView):
@@ -38,19 +42,22 @@ class NotificationView(viewsets.ModelViewSet):
         return NotificationToken.objects.filter(user=self.request.user)
 
 
-# class SendNotification(APIView):
-#     """Sends push notification alert if one exists."""
+class NotificationAlertView(APIView):
+    """
+    post: 
+    sends push notification alert if one exists
+    """
 
-#     permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
-#     def post(self, request):
-#         title = request.data["title"]
-#         body = request.data["body"]
+    def post(self, request):
+        title = request.data["title"]
+        body = request.data["body"]
 
-#         # only iOS for now
-#         token = get_object_or_404(
-#             NotificationToken, user=self.request.user, kind=NotificationToken.KIND_IOS
-#         )
+        # only iOS for now
+        token = get_object_or_404(
+            NotificationToken, user=self.request.user, kind=NotificationToken.KIND_IOS
+        )
 
-#         send_push_notification(token.token, title, body)
-#         return Response({"success": True})
+        send_push_notification(token.token, title, body)
+        return Response({"success": True})
