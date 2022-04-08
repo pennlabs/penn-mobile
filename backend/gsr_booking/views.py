2264e858585f01d81f327812c0f6eb46b2741551
Justin Zhang <judtin@seas.upenn.edu>
2022-04-08 16:26:37 -0400

    removed booking logic in favor of group logic

--- backend/gsr_booking/views.py
+++ backend/gsr_booking/views.py
@@ -10,7 +10,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from gsr_booking.api_wrapper import APIError, BookingWrapper
-from gsr_booking.booking_logic import book_rooms_for_group
 from gsr_booking.group_logic import GroupBook
 from gsr_booking.models import GSR, Group, GroupMembership, GSRBooking
 from gsr_booking.serializers import (
@@ -254,19 +253,13 @@ class GroupViewSet(viewsets.ModelViewSet):
         if not booking_serialized.is_valid():
             return Response(status=400)
 
-        booking_data = booking_serialized.data
-
         group = get_object_or_404(Group, pk=pk)
 
         # must be admin (and also a member) of the group to book
         if not group.has_admin(request.user):
             return HttpResponseForbidden()
 
-        result_json = book_rooms_for_group(
-            group, booking_data["room_bookings"], request.user.username
-        )
-
-        return Response(result_json)
+        return Response()
 
 
 # umbrella class used for accessing GSR API's (needed for token authentication)
