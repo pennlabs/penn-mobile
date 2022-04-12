f732f8c96704f4256ce2f37bc5681510818a137d
Joy Liu <joyliu.q@gmail.com>
2022-04-12 13:57:42 -0400

    :tada: Set-up

---
+++ k8s/main.ts
@@ -0,0 +1,60 @@
+import { Construct } from 'constructs';
+import { App } from 'cdk8s';
+import { PennLabsChart, ReactApplication, DjangoApplication, CronJob } from '@pennlabs/kittyhawk';
+
+const cronTime = require('cron-time-generator');
+
+export class MyChart extends PennLabsChart {
+  constructor(scope: Construct) {
+    super(scope);
+
+    const secret = "penn-mobile";
+    const backendImage = "pennlabs/penn-mobile-backend"
+    const frontendImage = "pennlabs/penn-mobile-frontend"
+
+    new DjangoApplication(this, 'django', {
+      deployment: {
+        image: backendImage,
+        secret,
+        replicas: 8,
+      },
+      domains: [
+        { host: 'studentlife.pennlabs.org', isSubdomain: true, paths: ['/'] },
+        { host: 'portal.pennmobile.org', isSubdomain: true, paths: ['/api', '/assets'] },
+        { host: 'pennmobile.org', paths: ['/api', '/assets'] },
+      ],
+      djangoSettingsModule: 'pennmobile.settings.production',
+    });
+
+    new ReactApplication(this, 'react', {
+      deployment: {
+        image: frontendImage,
+      },
+      domain: {
+        host: "portal.pennmobile.org",
+        isSubdomain: true,
+        paths: ['/']
+      },
+    });
+
+    new CronJob(this, 'get-laundry-snapshots', {
+      schedule: cronTime.every(15).minutes(),
+      image: backendImage,
+      secret,
+      cmd: ["python", "manage.py", "get_snapshot"],
+      env: [{ name: "DJANGO_SETTINGS_MODULE", value: "pennmobile.settings.production" }]
+    });
+
+    new CronJob(this, 'send-gsr-reminders', {
+      schedule: "20,50 * * * *",
+      image: backendImage,
+      secret,
+      cmd: ["python", "manage.py", "send_gsr_reminders"],
+      env: [{ name: "DJANGO_SETTINGS_MODULE", value: "pennmobile.settings.production" }]
+    });
+  }
+}
+
+const app = new App();
+new MyChart(app);
+app.synth();
