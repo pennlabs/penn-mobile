b7b22606a9f3d08643f8ccfe2cf9b68ddb996db3
Justin Zhang <judtin@seas.upenn.edu>
2022-04-14 02:00:55 -0400

    added comments

--- backend/penndata/management/commands/load_analytics.py
+++ backend/penndata/management/commands/load_analytics.py
@@ -16,23 +16,28 @@ class Command(BaseCommand):
 
         analytics_objects = []
 
+        # read in file and convert into array
         df = pd.read_csv("penndata/management/account.csv", header=None)
         np_arr = df.to_numpy()
 
         user_dict = dict()
 
+        # create mapping from pennkey to User to
+        # avoid db calls in the main loop of the function
         for user in User.objects.all():
             user_dict[user.username] = user
 
         for row in np_arr:
+            # iterate thru csv and add to list
             pennkey, created_at, cell_type, index, is_interaction, misc_2, data, misc_3 = row
 
+            # skips if User object not present
             if pennkey not in user_dict:
                 continue
 
+            # cleans csv data
             user = user_dict[pennkey]
             date_object = make_aware(datetime.strptime(created_at, "%Y-%m-%d %H:%M:%S.%f"))
-
             data = None if data == "NULL" else data
             is_interaction = True if is_interaction == 1 else False
 
@@ -47,6 +52,7 @@ class Command(BaseCommand):
                 )
             )
 
+        # bulk creates objects at once
         AnalyticsEvent.objects.bulk_create(analytics_objects)
 
         self.stdout.write("Uploaded Analytics Events!")
