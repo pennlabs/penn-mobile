91102b7f1e50ab16eec519af6b6783b990deebe0
Joy Liu <34288846+joyliu-q@users.noreply.github.com>
2022-05-12 22:06:10 -0700

    Use Kittyhawk for Deployment Config (#101)
    * :tada: Set-up
    
    * :art: Uniform Handling of DOMAINS env variable
    
    * :arrow_up: Bump Kittyhawk
    
    * :new: Add Get Fitness Snapshot Cronjob
    
    * :fire: Bye jest

--- backend/pennmobile/settings/base.py
+++ backend/pennmobile/settings/base.py
@@ -12,7 +12,7 @@ import os
 import dj_database_url
 
 
-DOMAINS = os.environ.get("DOMAINS", "example.com")
+DOMAINS = os.environ.get("DOMAINS", "example.com").split(",")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
