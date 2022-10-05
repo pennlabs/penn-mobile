ac26a06fb2afabcdb6bde0392d8bb2a81267c983
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2022-10-05 15:43:07 -0400

    Notif Bug Fixes (#118)
    * fix: update dev setting for tokens
    * fix: update credentials
    * refactor: rename the model token object
    * test: fix testcases

--- backend/tests/user/test_notifs.py
+++ backend/tests/user/test_notifs.py
@@ -40,6 +40,8 @@ class TestNotificationToken(TestCase):
         self.assertEqual(self.test_user, NotificationToken.objects.all().first().user)
 
     def test_create_update_token(self):
+        NotificationToken.objects.all().delete()
+
         # test that creating token returns correct response
         payload = {"kind": "IOS", "dev": "false", "token": "test123"}
         response = self.client.post("/user/notifications/tokens/", payload)
@@ -47,18 +49,20 @@ class TestNotificationToken(TestCase):
         self.assertEqual("IOS", res_json["kind"])
         self.assertFalse(res_json["dev"])
         self.assertEqual("test123", res_json["token"])
-        self.assertEqual(3, len(res_json))
+        self.assertEqual(4, len(res_json))
         self.assertEqual(1, NotificationToken.objects.all().count())
 
-        # new payload, should not create a new token
-        new_payload = {"kind": "IOS", "dev": "false", "token": "newtoken"}
-        response = self.client.post("/user/notifications/tokens/", new_payload)
+        # update token
+        new_payload = {"kind": "IOS", "token": "newtoken"}
+        response = self.client.patch(f"/user/notifications/tokens/{res_json['id']}/", new_payload)
         res_json = json.loads(response.content)
         self.assertEqual("newtoken", res_json["token"])
         self.assertEqual(1, NotificationToken.objects.all().count())
 
     def test_get_token(self):
-        # test that tokens are visible via GET
+        NotificationToken.objects.all().delete()
+
+        # create token
         payload = {"kind": "IOS", "dev": "false", "token": "test123"}
         response = self.client.post("/user/notifications/tokens/", payload)
 
@@ -68,7 +72,7 @@ class TestNotificationToken(TestCase):
         self.assertFalse(res_json[0]["dev"])
         self.assertEqual("test123", res_json[0]["token"])
         self.assertEqual(1, len(res_json))
-        self.assertEqual(3, len(res_json[0]))
+        self.assertEqual(4, len(res_json[0]))
         self.assertEqual(1, NotificationToken.objects.all().count())
 
 
@@ -90,26 +94,32 @@ class TestNotificationSetting(TestCase):
 
     def test_create_update_check_settings(self):
         # test that invalid settings are rejected
+        NotificationSetting.objects.filter(service="PENN_MOBILE").delete()
         payload = {"service": "Penn Mobile", "enabled": True}
         response = self.client.post("/user/notifications/settings/", payload)
         res_json = json.loads(response.content)
         self.assertNotEqual(res_json, payload)
 
-        # test that settings can be created and updated via POST
+        # test that settings can be created
         payload = {"service": "PENN_MOBILE", "enabled": True}
         response = self.client.post("/user/notifications/settings/", payload)
         res_json = json.loads(response.content)
-        self.assertEqual(res_json, payload)
+        self.assertEqual(res_json["service"], "PENN_MOBILE")
+        self.assertTrue(res_json["enabled"])
 
         # since token empty, should still return false
         response = self.client.get("/user/notifications/settings/PENN_MOBILE/check/")
         res_json = json.loads(response.content)
         self.assertFalse(res_json["enabled"])
-        self.assertTrue(res_json["missing_token"])
+
+        # since token empty, should still return false
+        response = self.client.get("/user/notifications/tokens/")
+        res_json = json.loads(response.content)
+        token_id = res_json[0]["id"]
 
         # update token to nonempty value
         payload = {"kind": "IOS", "dev": False, "token": "test123"}
-        response = self.client.post("/user/notifications/tokens/", payload)
+        response = self.client.put(f"/user/notifications/tokens/{token_id}/", payload)
         res_json = json.loads(response.content)
         self.assertEqual("test123", res_json["token"])
         self.assertEqual(1, NotificationToken.objects.all().count())
@@ -118,7 +128,6 @@ class TestNotificationSetting(TestCase):
         response = self.client.get("/user/notifications/settings/PENN_MOBILE/check/")
         res_json = json.loads(response.content)
         self.assertTrue(res_json["enabled"])
-        self.assertFalse(res_json["missing_token"])
 
     def test_check_fail(self):
         # since invalid setting, should return error
@@ -133,37 +142,31 @@ class TestNotificationAlert(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-        # create user2
-        self.test_user = User.objects.create_user("user2", "user2@seas.upenn.edu", "user2")
-        self.client.force_authenticate(user=self.test_user)
-        token = NotificationToken.objects.get(user=self.test_user)
-        token.token = "test234"
-        token.save()
-        setting = NotificationSetting.objects.get(token=token, service="PENN_MOBILE")
-        setting.enabled = True
-        setting.save()
-
         # create user1
         self.test_user = User.objects.create_user("user", "user@seas.upenn.edu", "user")
         self.client.force_authenticate(user=self.test_user)
-        token = NotificationToken.objects.get(user=self.test_user)
-        token.token = "test123"
-        token.save()
+        token_obj = NotificationToken.objects.get(user=self.test_user)
+        token_obj.token = "test123"
+        token_obj.save()
+
+        # create user2
+        self.test_user = User.objects.create_user("user2", "user2@seas.upenn.edu", "user2")
+        self.client.force_authenticate(user=self.test_user)
+        token_obj = NotificationToken.objects.get(user=self.test_user)
+        token_obj.token = "test234"
+        token_obj.save()
+        setting = NotificationSetting.objects.get(token=token_obj, service="PENN_MOBILE")
+        setting.enabled = True
+        setting.save()
 
     @mock.patch("user.views.send_push_notif", mock_send_notif)
     def test_single_notif(self):
         # test notif fail when setting is false
-        payload = {"title": "Test", "body": ":D", "service": "PENN_MOBILE"}
+        payload = {"title": "Test", "body": ":D", "service": "OHQ"}
         response = self.client.post("/user/notifications/alerts/", payload)
         res_json = json.loads(response.content)
         self.assertEqual(0, len(res_json["success_users"]))
         self.assertEqual(1, len(res_json["failed_users"]))
-
-        # update setting
-        payload = {"service": "PENN_MOBILE", "enabled": True}
-        response = self.client.post("/user/notifications/settings/", payload)
-        res_json = json.loads(response.content)
-        self.assertEqual(res_json, payload)
 
         # test notif success when setting is true
         payload = {"title": "Test", "body": ":D", "service": "PENN_MOBILE"}
@@ -174,11 +177,8 @@ class TestNotificationAlert(TestCase):
 
     @mock.patch("user.views.send_push_notif_batch", mock_send_notif)
     def test_batch_notif(self):
-        # update setting for first user
-        payload = {"service": "PENN_MOBILE", "enabled": True}
-        response = self.client.post("/user/notifications/settings/", payload)
-        res_json = json.loads(response.content)
-        self.assertEqual(res_json, payload)
+        # update all settings to be enabled
+        NotificationSetting.objects.all().update(enabled=True)
 
         # test notif
         payload = {
@@ -205,12 +205,12 @@ class TestSendGSRReminders(TestCase):
         self.client.force_authenticate(user=self.test_user)
 
         # enabling tokens and settings
-        token = NotificationToken.objects.get(user=self.test_user)
-        token.token = "test123"
-        token.save()
+        token_obj = NotificationToken.objects.get(user=self.test_user)
+        token_obj.token = "test123"
+        token_obj.save()
 
         setting = NotificationSetting.objects.get(
-            token=token, service=NotificationSetting.SERVICE_GSR_BOOKING
+            token=token_obj, service=NotificationSetting.SERVICE_GSR_BOOKING
         )
         setting.enabled = True
         setting.save()
@@ -253,9 +253,9 @@ class TestSendShadowNotifs(TestCase):
         self.client = APIClient()
         self.test_user = User.objects.create_user("user", "user@seas.upenn.edu", "user")
         self.client.force_authenticate(user=self.test_user)
-        token = NotificationToken.objects.get(user=self.test_user)
-        token.token = "test123"
-        token.save()
+        token_obj = NotificationToken.objects.get(user=self.test_user)
+        token_obj.token = "test123"
+        token_obj.save()
 
     @mock.patch(
         "user.management.commands.send_shadow_notifs.send_shadow_push_notif_batch", mock_send_notif
@@ -277,15 +277,15 @@ class TestCallingNotificationsAPI(TestCase):
         self.client = APIClient()
         self.test_user = User.objects.create_user("user", "user@seas.upenn.edu", "user")
         self.client.force_authenticate(user=self.test_user)
-        token = NotificationToken.objects.get(user=self.test_user)
-        token.token = "test123"
-        token.save()
+        token_obj = NotificationToken.objects.get(user=self.test_user)
+        token_obj.token = "test123"
+        token_obj.save()
 
     @mock.patch("os.path.join", mock_get_path)
     @mock.patch("apns2.client.APNsClient.send_notification", mock_send_notif)
     def test_send_push_notif(self):
-        token = NotificationToken.objects.first()
-        notif.send_push_notif(token=token, title="title", body="body")
+        token_obj = NotificationToken.objects.first()
+        notif.send_push_notif(token=token_obj, title="title", body="body")
 
     @mock.patch("os.path.join", mock_get_path)
     @mock.patch("apns2.client.APNsClient.send_notification_batch", mock_send_notif)
