208122e7df7d55ced915fed13c493fdef4970e57
Justin Zhang <judtin@seas.upenn.edu>
2022-10-08 23:27:16 -0400

    Added tests, removed legacy code, added option to select menu based on date

--- backend/dining/admin.py
+++ backend/dining/admin.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
 
-from dining.models import (
-    DiningBalance,
-    DiningItem,
-    DiningMenu,
-    DiningStation,
-    DiningTransaction,
-    Venue,
-)
+from dining.models import DiningItem, DiningMenu, DiningStation, Venue
 
 
 admin.site.register(Venue)
-admin.site.register(DiningTransaction)
-admin.site.register(DiningBalance)
 admin.site.register(DiningItem)
 admin.site.register(DiningMenu)
 admin.site.register(DiningStation)
--- backend/dining/models.py
+++ backend/dining/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-from user.models import Profile
-
 
 class Venue(models.Model):
     venue_id = models.IntegerField(primary_key=True)
@@ -10,29 +8,7 @@ class Venue(models.Model):
     image_url = models.URLField()
 
     def __str__(self):
-        return str(self.venue_id)
-
-
-class DiningTransaction(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    date = models.DateTimeField()
-    description = models.TextField()
-    amount = models.FloatField()
-    balance = models.FloatField(default=0)
-
-    def __str__(self):
-        return f"{self.profile} - {self.date} - {self.amount}"
-
-
-class DiningBalance(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    date = models.DateTimeField(default=timezone.now)
-    dining_dollars = models.FloatField()
-    swipes = models.IntegerField()
-    guest_swipes = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.profile} - {self.date}"
+        return f"{self.name}-{str(self.venue_id)}"
 
 
 class DiningItem(models.Model):
