204bc623dc51ab97a63eb258d51d4ec5a41ad619
Justin Zhang <judtin@seas.upenn.edu>
2022-10-09 17:07:42 -0400

    Revert "[Dining API Fix] Menus (#115)"
    This reverts commit 33fe94e94dba077511f61fb37cc601d54c12acd4.

--- backend/dining/models.py
+++ backend/dining/models.py
@@ -1,32 +1,34 @@
 from django.db import models
 from django.utils import timezone
 
+from user.models import Profile
+
 
 class Venue(models.Model):
     venue_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255, null=True)
     image_url = models.URLField()
 
     def __str__(self):
-        return f"{self.name}-{str(self.venue_id)}"
+        return str(self.venue_id)
 
 
-class DiningItem(models.Model):
-    item_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    ingredients = models.CharField(max_length=255)
+class DiningTransaction(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    description = models.TextField()
+    amount = models.FloatField()
+    balance = models.FloatField(default=0)
+
+    def __str__(self):
+        return f"{self.profile} - {self.date} - {self.amount}"
 
 
-class DiningStation(models.Model):
-    name = models.CharField(max_length=255)
-    items = models.ManyToManyField(DiningItem)
+class DiningBalance(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+    dining_dollars = models.FloatField()
+    swipes = models.IntegerField()
+    guest_swipes = models.IntegerField()
 
-
-class DiningMenu(models.Model):
-    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
-    date = models.DateField(default=timezone.now)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    stations = models.ManyToManyField(DiningStation)
-    service = models.CharField(max_length=255)
+    def __str__(self):
+        return f"{self.profile} - {self.date}"
