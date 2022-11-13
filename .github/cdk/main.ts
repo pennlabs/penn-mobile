931328c7a2575f07c7753bdfb8977e6032f92baf
Joy Liu <34288846+joyliu-q@users.noreply.github.com>
2022-11-13 12:24:29 -0500

    Upgrade Backend Deps (Python 3.9.14) (#148)
    * :tada: Upgrade to Python 3.9.14
    
    * :arrow_up: Updated deps
    
    * :art: Update image
    
    * :art: Bruh
    
    * :sweat: Sad
    
    * :art: gen the lock
    
    * :art: :O
    
    * :art: SeqUentiAL?
    
    * :fire: Remove MySQLClient

--- .github/cdk/main.ts
+++ .github/cdk/main.ts
@@ -5,6 +5,9 @@ const app = new App();
 new LabsApplicationStack(app, {
   djangoProjectName: 'pennmobile',
   dockerImageBaseName: 'penn-mobile',
+  djangoCheckProps: {
+    pythonVersion: "3.9.14-buster",
+  }
 });
 
 app.synth();
