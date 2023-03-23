d5298b48bcf5adad855c3afbe011ed7b7aa6456c
Vincent Cai <72774400+vcai122@users.noreply.github.com>
2023-03-22 20:00:49 -0400

    Fitness Hourly Usage Route (#173)
    * implement route for fitness usage
    
    * linting + description for get
    
    * final changes: tests + address pr comments
    
    * linting
    
    * fix failing test case for get_fitness_snapshot
    
    * change variable names
    
    * linting

--- backend/penndata/management/commands/get_fitness_snapshot.py
+++ backend/penndata/management/commands/get_fitness_snapshot.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
@@ -6,20 +8,24 @@ from django.utils import timezone
 from penndata.models import FitnessRoom, FitnessSnapshot
 
 
-def get_capacities():
+def get_usages():
 
-    # capacities default to 0 because spreadsheet number appears blank if 0 people at location
-    capacities = {
-        "4th Floor Fitness": 0,
-        "3rd Floor Fitness": 0,
-        "2nd Floor Strength": 0,
-        "Basketball Courts": 0,
-        "MPR": 0,
-        "Climbing Wall": 0,
-        "1st floor Fitness": 0,
-        "Pool-Shallow": 0,
-        "Pool-Deep": 0,
-    }
+    # count/capacities default to 0 since spreadsheet number appears blank if no one there
+    locations = [
+        "4th Floor Fitness",
+        "3rd Floor Fitness",
+        "2nd Floor Strength",
+        "Basketball Courts",
+        "MPR",
+        "Climbing Wall",
+        "1st floor Fitness",
+        "Pool-Shallow",
+        "Pool-Deep",
+    ]
+    usages = {location: {"count": 0, "capacity": 0} for location in locations}
+
+    date = timezone.localtime()  # default if can't get date from spreadsheet
+
     try:
         resp = requests.get(
             (
@@ -33,41 +39,46 @@ def get_capacities():
         return None
 
     html = resp.content.decode("utf8")
-
     soup = BeautifulSoup(html, "html5lib")
+    if not (embedded_spreadsheet := soup.find("tbody")):
+        return None
 
-    embedded_spreadsheet = soup.find("body", {"class": "docs-gm"})
     table_rows = embedded_spreadsheet.findChildren("tr")
-    for row in table_rows:
+    for i, row in enumerate(table_rows):
         cells = row.findChildren("td")
-        if len(cells) >= 2:
-            location = cells[0].getText()
-            if location in capacities:
-                try:
-                    count = int(cells[1].getText())
-                    capacities[location] = count
-                except ValueError:
-                    capacities[location] = 0
-
-    return capacities
+        if i == 0:
+            date = timezone.make_aware(datetime.strptime(cells[1].getText(), "%m/%d/%Y %H:%M:%S"))
+        elif (location := cells[0].getText()) in usages:
+            try:
+                count = int(cells[1].getText())
+                capacity = float(cells[2].getText().strip("%"))
+                usages[location] = {"count": count, "capacity": capacity}
+            except ValueError:
+                pass
+    return usages, date
 
 
 class Command(BaseCommand):
     help = "Captures a new Fitness Snapshot for every Laundry room."
 
     def handle(self, *args, **kwargs):
-        now = timezone.localtime()
+        usage_by_location, date = get_usages()
 
         # prevent double creating FitnessSnapshots
-        if FitnessSnapshot.objects.filter(date=now).count() == 0:
+        if FitnessSnapshot.objects.filter(date=date).exists():
+            self.stdout.write("FitnessSnapshots already exist for this date!")
+            return
 
-            data = get_capacities()
-
-            for room_name, count in data.items():
-
-                fitness_room = FitnessRoom.objects.get(name=room_name)
-
-                FitnessSnapshot.objects.create(
-                    room=fitness_room, date=now, count=count,
+        FitnessSnapshot.objects.bulk_create(
+            [
+                FitnessSnapshot(
+                    room=FitnessRoom.objects.get_or_create(name=room_name)[0],
+                    date=date,
+                    count=room_usage["count"],
+                    capacity=room_usage["capacity"],
                 )
+                for room_name, room_usage in usage_by_location.items()
+            ]
+        )
+
         self.stdout.write("Captured fitness snapshots!")
