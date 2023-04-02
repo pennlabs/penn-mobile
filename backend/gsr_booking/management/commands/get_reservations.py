629e7b39992c60c611fd99b5dd77c77a7c90900f
vcai122 <vincent.delaware@gmail.com>
2023-04-02 13:07:29 -0400

    add option to get unique users

--- backend/gsr_booking/management/commands/get_reservations.py
+++ backend/gsr_booking/management/commands/get_reservations.py
@@ -1,10 +1,11 @@
-import datetime
+from datetime import datetime
 
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 from django.db.models import Q
+from django.utils import timezone
 
-from backend.gsr_booking.models import Reservation
+from gsr_booking.models import Reservation
 from gsr_booking.models import Group
 
 
@@ -22,6 +23,7 @@ class Command(BaseCommand):
     --end       flag to specify end date; expected format: YYYY-MM-DD
     --current   flag to specify current reservations
     --time      flag to specify total time of reservations
+    --user      flag to specify to get number of unique users
 
     Note: --start/--end and --current are mutually exclusive
     """
@@ -29,10 +31,12 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         # optional flags
         parser.add_argument("--group", type=str, default=None)
-        parser.add_argument("--start", type=datetime, default=None)
+        parser.add_argument("--start", type=str, default=None)
         parser.add_argument("--end", type=str, default=None)
         parser.add_argument("--current", type=bool, default=False)
         parser.add_argument("--time", type=bool, default=False)
+        parser.add_argument("--user", type=bool, default=False)
+        
 
     def handle(self, *args, **kwargs):
         group = kwargs["group"]
@@ -40,6 +44,7 @@ class Command(BaseCommand):
         end = kwargs["end"]
         current = kwargs["current"]
         time = kwargs["time"]
+        user = kwargs["user"]
 
         if start and not (start := self.__convert_date(start)):
             self.stdout.write("Error: invalid start date format")
@@ -74,6 +79,10 @@ class Command(BaseCommand):
                 / 3600
             )
             self.stdout.write(f"Total time: {total_time}")
+        if user:
+            users = reservations.values_list("creator", flat=True).distinct()
+            self.stdout.write(f"Number of unique users: {users.count()}")
+
         self.stdout.write(f"Number of reservations: {reservations.count()}")
 
     def __convert_date(self, date_str):
@@ -84,7 +93,8 @@ class Command(BaseCommand):
         :param date_str: string in format YYYY-MM-DD
         :return: datetime object
         """
+        
         try:
-            return datetime.strptime(date_str, "%Y-%m-%d")
+            return timezone.make_aware(datetime.strptime(date_str, "%Y-%m-%d"))
         except ValueError:
             return None
