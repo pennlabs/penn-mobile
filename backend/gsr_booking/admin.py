a9918778c4da4300271ddb21414b0c270f5c01e5
Justin Zhang <judtin@seas.upenn.edu>
2023-09-06 15:12:37 -0400

    Added search functionality for admins for group gsrs

--- backend/gsr_booking/admin.py
+++ backend/gsr_booking/admin.py
@@ -3,8 +3,12 @@ from django.contrib import admin
 from gsr_booking.models import GSR, Group, GroupMembership, GSRBooking, Reservation
 
 
+class GroupMembershipAdmin(admin.ModelAdmin):
+    search_fields = ["user__username__icontains", "group__name__icontains"]
+
+
 admin.site.register(Group)
-admin.site.register(GroupMembership)
+admin.site.register(GroupMembership, GroupMembershipAdmin)
 admin.site.register(GSR)
 admin.site.register(GSRBooking)
 admin.site.register(Reservation)
