c731ff1b962277a1eaf4a89e926c8c3179cd2c81
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-09-15 13:13:19 -0400

    feat: caching for most laundry status routes

--- backend/laundry/urls.py
+++ backend/laundry/urls.py
@@ -1,13 +1,42 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+from backend.utils.cache import MINUTE_IN_SECONDS, MONTH_IN_SECONDS_APPROX
 
-from laundry.views import HallInfo, HallUsage, Ids, MultipleHallInfo, Preferences, Status
+from laundry.views import (
+    HallInfo,
+    HallUsage,
+    Ids,
+    MultipleHallInfo,
+    Preferences,
+    Status,
+)
 
 
 urlpatterns = [
-    path("hall/<hall_id>/", HallInfo.as_view(), name="hall-info"),
-    path("usage/<hall_id>/", HallUsage.as_view(), name="hall-usage"),
-    path("rooms/<hall_ids>", MultipleHallInfo.as_view(), name="multiple-hall-info"),
-    path("halls/ids/", Ids.as_view(), name="hall-ids"),
-    path("status/", Status.as_view(), name="status"),
+    path(
+        "hall/<hall_id>/",
+        cache_page(MINUTE_IN_SECONDS)(HallInfo),
+        name="hall-info",
+    ),
+    path(
+        "usage/<hall_id>/",
+        cache_page(MINUTE_IN_SECONDS)(HallUsage),
+        name="hall-usage",
+    ),
+    path(
+        "rooms/<hall_ids>",
+        cache_page(MINUTE_IN_SECONDS)(MultipleHallInfo),
+        name="multiple-hall-info",
+    ),
+    path(
+        "halls/ids/",
+        cache_page(MONTH_IN_SECONDS_APPROX)(Ids),
+        name="hall-ids",
+    ),
+    path(
+        "status/",
+        cache_page(30)(Status),
+        name="status",
+    ),
     path("preferences/", Preferences.as_view(), name="preferences"),
 ]
