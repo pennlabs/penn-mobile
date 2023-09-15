5f85b2717cbc454fd09b143ef6887856b3d98322
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-09-15 12:07:59 -0400

    feat: caching of some dining routes

--- backend/dining/urls.py
+++ backend/dining/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.decorators.cache import cache_page
+from backend.utils.cache import HOUR_IN_SECONDS, MONTH_IN_SECONDS_APPROX
 
 from dining.views import Menus, Preferences, Venues
 
@@ -7,10 +8,22 @@ from dining.views import Menus, Preferences, Venues
 urlpatterns = [
     path(
         "venues/",
-        cache_page(30 * 24 * 60 * 60)(Venues),
+        cache_page(1 * MONTH_IN_SECONDS_APPROX)(Venues),
         name="venues",
     ),
-    path("menus/", Menus.as_view(), name="menus"),
-    path("menus/<date>/", Menus.as_view(), name="menus-with-date"),
-    path("preferences/", Preferences.as_view(), name="dining-preferences"),
+    path(
+        "menus/",
+        cache_page(1 * HOUR_IN_SECONDS)(Menus),
+        name="menus",
+    ),
+    path(
+        "menus/<date>/",
+        cache_page(1 * HOUR_IN_SECONDS)(Menus),
+        name="menus-with-date",
+    ),
+    path(
+        "preferences/",
+        Preferences.as_view(),
+        name="dining-preferences",
+    ),
 ]
