ba87bb1bd67302ff9f21777abbaabc51d6da53dc
vcai122 <vincent.delaware@gmail.com>
2023-09-17 13:33:10 -0400

    linting

--- backend/penndata/admin.py
+++ backend/penndata/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
-from django.utils.html import escape
-from django.utils.html import mark_safe
-
+from django.utils.html import escape, mark_safe
 
 from penndata.models import AnalyticsEvent, Event, FitnessRoom, FitnessSnapshot, HomePageOrder
+
 
 # custom fitness room admin
 class FitnessRoomAdmin(admin.ModelAdmin):
     def image_tag(self, instance):
         return mark_safe('<img src="%s" height="300" />' % escape(instance.image_url))
-    image_tag.short_description = 'Image'
+
+    image_tag.short_description = "Image"
     image_tag.allow_tags = True
-    readonly_fields = ('image_tag',)
+    readonly_fields = ("image_tag",)
+
 
 admin.site.register(Event)
 admin.site.register(HomePageOrder)
