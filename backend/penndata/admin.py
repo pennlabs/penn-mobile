cd2c245d6ec9db7389190b9f640507be0a9bd66a
vcai122 <vincent.delaware@gmail.com>
2023-09-23 21:40:10 -0400

    add image for posts

--- backend/penndata/admin.py
+++ backend/penndata/admin.py
@@ -4,13 +4,11 @@ from django.utils.html import escape, mark_safe
 from penndata.models import AnalyticsEvent, Event, FitnessRoom, FitnessSnapshot, HomePageOrder
 
 
-# custom fitness room admin
 class FitnessRoomAdmin(admin.ModelAdmin):
     def image_tag(self, instance):
         return mark_safe('<img src="%s" height="300" />' % escape(instance.image_url))
 
-    image_tag.short_description = "Image"
-    image_tag.allow_tags = True
+    image_tag.short_description = "Fitness Room Image"
     readonly_fields = ("image_tag",)
 
 
