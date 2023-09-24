4960df4d0d06b0264384b7733dba13ae6e174fcb
Vincent Cai <72774400+vcai122@users.noreply.github.com>
2023-09-24 00:19:10 -0400

    Add Image to Fitness Admin and Portal Admin (#205)
    * Add Image to Fitness Admin for POC for Portal
    
    * linting
    
    * add image for posts

--- backend/penndata/admin.py
+++ backend/penndata/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from django.utils.html import escape, mark_safe
 
 from penndata.models import AnalyticsEvent, Event, FitnessRoom, FitnessSnapshot, HomePageOrder
 
 
+class FitnessRoomAdmin(admin.ModelAdmin):
+    def image_tag(self, instance):
+        return mark_safe('<img src="%s" height="300" />' % escape(instance.image_url))
+
+    image_tag.short_description = "Fitness Room Image"
+    readonly_fields = ("image_tag",)
+
+
 admin.site.register(Event)
 admin.site.register(HomePageOrder)
-admin.site.register(FitnessRoom)
+admin.site.register(FitnessRoom, FitnessRoomAdmin)
 admin.site.register(FitnessSnapshot)
 admin.site.register(AnalyticsEvent)
