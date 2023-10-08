cb30ec072cf9ab80b364ac58fb72f6d98720d455
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-10-08 14:44:47 -0400

    format: apply linters

--- backend/laundry/urls.py
+++ backend/laundry/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path("hall/<hall_id>/", cache_page(Cache.MINUTE)(HallInfo.as_view()), name="hall-info",),
     path("usage/<hall_id>/", cache_page(Cache.MINUTE)(HallUsage.as_view()), name="hall-usage",),
     path(
-        "rooms/<hall_ids>", cache_page(Cache.MINUTE)(MultipleHallInfo.as_view()), name="multiple-hall-info",
+        "rooms/<hall_ids>",
+        cache_page(Cache.MINUTE)(MultipleHallInfo.as_view()),
+        name="multiple-hall-info",
     ),
     path("halls/ids/", cache_page(Cache.MONTH)(Ids.as_view()), name="hall-ids",),
     path("status/", Status.as_view(), name="status"),
