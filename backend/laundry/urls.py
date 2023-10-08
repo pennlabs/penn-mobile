dd92b01e7c2191a871f7dc644e59bd29cd6a9f97
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-10-08 14:43:00 -0400

    fix: pass in

--- backend/laundry/urls.py
+++ backend/laundry/urls.py
@@ -6,12 +6,12 @@ from utils.cache import Cache
 
 
 urlpatterns = [
-    path("hall/<hall_id>/", cache_page(Cache.MINUTE)(HallInfo), name="hall-info",),
-    path("usage/<hall_id>/", cache_page(Cache.MINUTE)(HallUsage), name="hall-usage",),
+    path("hall/<hall_id>/", cache_page(Cache.MINUTE)(HallInfo.as_view()), name="hall-info",),
+    path("usage/<hall_id>/", cache_page(Cache.MINUTE)(HallUsage.as_view()), name="hall-usage",),
     path(
-        "rooms/<hall_ids>", cache_page(Cache.MINUTE)(MultipleHallInfo), name="multiple-hall-info",
+        "rooms/<hall_ids>", cache_page(Cache.MINUTE)(MultipleHallInfo.as_view()), name="multiple-hall-info",
     ),
-    path("halls/ids/", cache_page(Cache.MONTH)(Ids), name="hall-ids",),
+    path("halls/ids/", cache_page(Cache.MONTH)(Ids.as_view()), name="hall-ids",),
     path("status/", Status.as_view(), name="status"),
     path("preferences/", Preferences.as_view(), name="preferences"),
 ]
