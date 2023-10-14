9bc0a2f6211ed660049bdd56ef5aba7f1b8cb5a4
Justin Zhang <judtin@seas.upenn.edu>
2023-10-13 22:47:39 -0400

    Update AWS s3 environment variable name for new django-storages version

--- backend/pennmobile/settings/base.py
+++ backend/pennmobile/settings/base.py
@@ -171,7 +171,7 @@ WHARTON_TOKEN = os.environ.get("WHARTON_TOKEN", None)
 # Upload file storage
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", None)
-AWS_ACCESS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
 AWS_STORAGE_BUCKET_NAME = "penn.mobile.portal"
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
