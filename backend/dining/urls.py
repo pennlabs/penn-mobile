5378ea7c5158ddf8f513819d842d310c52a31dae
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-12-01 18:03:01 -0500

    feat: finish caching

--- backend/dining/urls.py
+++ backend/dining/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from dining.views import Menus, Preferences, Venues
+from utils.cache import Cache
 
 
 urlpatterns = [
-    path("venues/", Venues.as_view(), name="venues"),
-    path("menus/", Menus.as_view(), name="menus"),
-    path("menus/<date>/", Menus.as_view(), name="menus-with-date"),
+    path("venues/", cache_page(12 * Cache.HOUR)(Venues.as_view()), name="venues"),
+    path("menus/", cache_page(3 * Cache.HOUR)(Menus.as_view()), name="menus"),
+    path("menus/<date>/", cache_page(3 * Cache.HOUR)(Menus.as_view()), name="menus-with-date"),
     path("preferences/", Preferences.as_view(), name="dining-preferences"),
 ]
