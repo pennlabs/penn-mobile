8e8f1b0e3da00f1ccab86716224186ceac9dca7f
Justin Zhang <judtin@seas.upenn.edu>
2024-02-08 18:32:18 -0500

    Improved get_calendar

--- backend/penndata/management/commands/get_calendar.py
+++ backend/penndata/management/commands/get_calendar.py
@@ -3,19 +3,27 @@ import datetime
 import requests
 from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from penndata.models import CalendarEvent
 
+UPENN_ALMANAC_WEBSITE = "https://almanac.upenn.edu/penn-academic-calendar"
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        # scrapes almanac from upenn website
+
+        # Clears out previous CalendarEvents
+        CalendarEvent.objects.all().delete()
+        
+        # Scrapes UPenn Almanac
         try:
-            resp = requests.get("https://almanac.upenn.edu/penn-academic-calendar")
+            resp = requests.get(UPENN_ALMANAC_WEBSITE)
         except ConnectionError:
             return None
+
         soup = BeautifulSoup(resp.content.decode("utf8"), "html5lib")
-        # finds table with all information and gets the rows
+
+        # Relevant Table class
         table = soup.find(
             "table",
             {
@@ -25,53 +33,43 @@ class Command(BaseCommand):
                 )
             },
         )
+
         rows = table.find_all("tr")
-        current_time = datetime.datetime.now()
+        current_time = timezone.localtime()
         current_year = current_time.year
         row_year = 0
-        # collect end dates on all events and filter based on that
+
         for row in rows:
             header = row.find_all("th")
+
+            # Gets data from header
             if len(header) > 0:
                 row_year = header[0].get_text().split(" ")[0]
-            # skips calculation if years don't align
+                continue
+
+            # Only get data from relevant year
             if int(row_year) != current_year:
                 continue
-            if len(header) == 0:
-                data = row.find_all("td")
-                date_info = data[1].get_text()
-                date = None
-                try:
-                    # handles case for date ex. August 31
-                    date = datetime.datetime.strptime(
-                        date_info + str(current_year) + "-04:00", "%B %d%Y%z"
-                    )
-                except ValueError:
-                    try:
-                        # handles case for date ex. August 1-3
-                        month = date_info.split(" ")[0]
-                        day = date_info.split("-")[1]
-                        date = datetime.datetime.strptime(
-                            month + day + str(current_year) + "-04:00", "%B%d%Y%z"
-                        )
-                    except (ValueError, IndexError):
-                        try:
-                            # handles case for date ex. August 1-September 31
-                            last_date = date_info.split("-")[0].split(" ")
-                            month = last_date[0]
-                            day = last_date[1]
-                            date = datetime.datetime.strptime(
-                                month + day + str(current_year) + "-04:00", "%B%d%Y%z"
-                            )
-                        except (ValueError, IndexError):
-                            pass
 
-                if date and current_time.replace(tzinfo=None) < date.replace(tzinfo=None) < (
-                    current_time + datetime.timedelta(days=30)
-                ).replace(tzinfo=None):
+            data = row.find_all("td")    
+            event = data[0].get_text() 
+            date_info = data[1].get_text()
 
-                    CalendarEvent.objects.get_or_create(
-                        event=data[0].get_text(), date=date_info, date_obj=date
-                    )
-
+            """
+            Match works for different date types; always matches begin date:
+            - Range date in same month: August 1-3
+            - Range date across months: August 1-September 1
+            - Single date: August 1 
+            """
+            try:
+                month = date_info.split(" ")[0]
+                day = date_info.split(" ")[1].split("-")[0]
+                date = datetime.datetime.strptime(
+                    month + day + str(current_year) + "-04:00", "%B%d%Y%z"
+                )
+                if date and date >= timezone.localtime(): 
+                    CalendarEvent.objects.get_or_create(event=event, date=date_info, date_obj=date) 
+            except ValueError:
+                continue
+                
         self.stdout.write("Uploaded Calendar Events!")
