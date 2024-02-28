2ffaa1fab98d2652b47dce066d55476315449f70
Justin Zhang <judtin@seas.upenn.edu>
2024-02-27 21:42:26 -0500

    Lint

--- backend/penndata/management/commands/get_events.py
+++ backend/penndata/management/commands/get_events.py
@@ -60,9 +60,7 @@ class Command(BaseCommand):
 
             if start_date_str == "02/29" or start_date_str == "2/29":
                 # If it's February 29th
-                start_date = datetime.datetime.strptime("02/28", "%m/%d").replace(
-                    year=current_year
-                )
+                start_date = datetime.datetime.strptime("02/28", "%m/%d").replace(year=current_year)
                 if start_date.month < current_month:
                     # If scraped month is before current month, increment year
                     start_date = start_date.replace(year=current_year + 1)
@@ -101,8 +99,9 @@ class Command(BaseCommand):
                         - datetime.timedelta(seconds=1)
                     )
                 else:  # no end date or end time
-                    end_date = (start_date + datetime.timedelta(days=1)
-                                - datetime.timedelta(seconds=1))
+                    end_date = (
+                        start_date + datetime.timedelta(days=1) - datetime.timedelta(seconds=1)
+                    )
 
             Event.objects.update_or_create(
                 name=name,
@@ -129,8 +128,9 @@ class Command(BaseCommand):
         )
         end_time_soup = BeautifulSoup(event_element.get_attribute("innerHTML"), "html.parser")
 
-        end_time_range_str = end_time_soup.find(
-            "p", class_="event__meta event__time").text.strip().replace(".", "")
+        end_time_range_str = (
+            end_time_soup.find("p", class_="event__meta event__time").text.strip().replace(".", "")
+        )
         print(end_time_range_str)
         if not end_time_range_str or "all day" in end_time_range_str.lower():
             driver.quit()
