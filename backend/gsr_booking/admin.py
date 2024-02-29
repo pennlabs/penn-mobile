5af66e1f77cac2b81235c731036a910dd9b33e50
Vincent Cai <72774400+vcai122@users.noreply.github.com>
2024-02-28 19:45:45 -0500

    Fix libcal booking (#247)
    * fix libcal booking
    
    * add in_use field to gsr + make better admin page
    
    * fix tests
    
    * fix serializer

--- backend/gsr_booking/admin.py
+++ backend/gsr_booking/admin.py
@@ -7,8 +7,17 @@ class GroupMembershipAdmin(admin.ModelAdmin):
     search_fields = ["user__username__icontains", "group__name__icontains"]
 
 
+class GSRAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return GSR.all_objects.all()
+
+    list_display = ["name", "kind", "lid", "gid", "in_use"]
+    search_fields = ["name", "lid", "gid"]
+    ordering = ["-in_use"]
+
+
 admin.site.register(Group)
 admin.site.register(GroupMembership, GroupMembershipAdmin)
-admin.site.register(GSR)
+admin.site.register(GSR, GSRAdmin)
 admin.site.register(GSRBooking)
 admin.site.register(Reservation)
