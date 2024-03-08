85d50b6a5341c4d5dc10f5d6b4b0675a2ab95e70
Justin Zhang <judtin@seas.upenn.edu>
2024-03-08 00:04:34 -0500

    Lint

--- backend/penndata/migrations/0006_fitnesssnapshot_capacity.py
+++ backend/penndata/migrations/0006_fitnesssnapshot_capacity.py
@@ -11,8 +11,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
-            model_name="fitnesssnapshot",
-            name="capacity",
-            field=models.FloatField(null=True),
+            model_name="fitnesssnapshot", name="capacity", field=models.FloatField(null=True),
         ),
     ]
--- backend/penndata/migrations/0010_auto_20240228_0150.py
+++ backend/penndata/migrations/0010_auto_20240228_0150.py
@@ -10,14 +10,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name="event",
-            name="facebook",
-        ),
+        migrations.RemoveField(model_name="event", name="facebook",),
         migrations.AlterField(
-            model_name="event",
-            name="description",
-            field=models.TextField(blank=True, null=True),
+            model_name="event", name="description", field=models.TextField(blank=True, null=True),
         ),
         migrations.AlterField(
             model_name="event",
@@ -25,9 +20,7 @@ class Migration(migrations.Migration):
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
         migrations.AlterField(
-            model_name="event",
-            name="end",
-            field=models.DateTimeField(blank=True, null=True),
+            model_name="event", name="end", field=models.DateTimeField(blank=True, null=True),
         ),
         migrations.AlterField(
             model_name="event",
@@ -35,9 +28,7 @@ class Migration(migrations.Migration):
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
         migrations.AlterField(
-            model_name="event",
-            name="image_url",
-            field=models.URLField(blank=True, null=True),
+            model_name="event", name="image_url", field=models.URLField(blank=True, null=True),
         ),
         migrations.AlterField(
             model_name="event",
