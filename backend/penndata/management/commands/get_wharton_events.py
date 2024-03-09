2a431ef2f0750efe104e904b5e476ce0ea217062
ashleyzhang01 <ashleyzhang10@gmail.com>
2024-03-09 01:38:31 -0500

    lint

--- backend/penndata/management/commands/get_wharton_events.py
+++ backend/penndata/management/commands/get_wharton_events.py
@@ -1,6 +1,6 @@
 import datetime
-
 import re
+
 import requests
 from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
@@ -14,34 +14,34 @@ WHARTON_EVENTS_WEBSITE = "https://events.wharton.upenn.edu/events-hq/#list"
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        now = timezone.localtime()
+        # now = timezone.localtime()
 
         try:
             resp = requests.get(WHARTON_EVENTS_WEBSITE)
         except ConnectionError:
             print("Error:", ConnectionError)
             return None
-        
         soup = BeautifulSoup(resp.content, "html.parser")
 
-        event_entries = soup.find_all(class_='post-entry')
+        event_entries = soup.find_all(class_="post-entry")
 
         for entry in event_entries:
-            title = entry.find(class_='entry-title').text.strip()
-            description = entry.find('p').text.strip()
-            link = entry.find(class_='entry-title').a['href']
+            title = entry.find(class_="entry-title").text.strip()
+            description = entry.find("p").text.strip()
+            link = entry.find(class_="entry-title").a["href"]
 
-            info = entry.find(class_='info').span.text.strip()
-            match = re.match(r'(\w+\s+\d+) \| (\d{1,2}:\d{2} [AP]M) - (\d{1,2}:\d{2} [AP]M)', info)
+            info = entry.find(class_="info").span.text.strip()
+            match = re.match(
+                r"(\w+\s+\d+) \| (\d{1,2}:\d{2} [AP]M) - (\d{1,2}:\d{2} [AP]M)", info
+            )
             if match:
                 _, start_time, end_time = match.groups()
-                start_time_obj = datetime.datetime.strptime(start_time, '%I:%M %p')
-                end_time_obj = datetime.datetime.strptime(end_time, '%I:%M %p')
+                start_time_obj = datetime.datetime.strptime(start_time, "%I:%M %p")
+                end_time_obj = datetime.datetime.strptime(end_time, "%I:%M %p")
             else:
                 print("Error: Cannot find date, update scraper.")
                 return
-
-            location = ' '.join([info.split('•')[-2], info.split('•')[-1]])
+            location = ",".join([info.split("•")[-2], info.split("•")[-1]])
             Event.objects.update_or_create(
                 name=title,
                 defaults={
@@ -55,5 +55,4 @@ class Command(BaseCommand):
                     "email": "",
                 },
             )
-
         self.stdout.write("Uploaded Wharton Events!")
