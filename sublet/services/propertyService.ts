25258633511e431849ea4998c6fa903dc5330e8f
jonrxu <jonrxu@gmail.com>
2024-03-29 16:45:23 -0400

    added create property image

--- sublet/services/propertyService.ts
+++ sublet/services/propertyService.ts
@@ -93,6 +93,57 @@ export const createProperty = async (property: any): Promise<any> => {
   }
 };
 
+export const createPropertyImage = async (sublet_id: string, image: any): Promise<any> => {
+  // Retrieve username and password from environment variables
+  const username = process.env.NEXT_PUBLIC_API_USERNAME;
+  const password = process.env.NEXT_PUBLIC_API_PASSWORD;
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/properties/${sublet_id}/images`
+
+  if (!username || !password) {
+    console.error('API credentials are not set in environment variables.');
+    throw new Error('API credentials are missing.');
+  }
+
+  const encodedCredentials = btoa(`${username}:${password}`);
+
+  const csrfToken = getCsrfTokenCookie();
+
+  // Set up the HTTP headers with Basic Authentication
+  const headers = new Headers({
+    'Authorization': `Basic ${encodedCredentials}`,
+    'Content-Type': 'application/json',
+    'X-CSRFToken': csrfToken
+  });
+
+  // Make the fetch request with the headers
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({
+      sublet: sublet_id,
+      image: image
+    }),
+  });
+
+  // Check if the response was not ok
+  if (!response.ok) {
+    // Attempt to parse the error response body
+    let errorMsg = `Error ${response.status}: ${response.statusText}`;
+    try {
+      const errorBody = await response.json();
+      errorMsg += `: ${errorBody.detail || JSON.stringify(errorBody)}`;
+    } catch (e) {
+      // If there's an error parsing the error response, log it
+      console.error('Error parsing the error response:', e);
+    }
+
+    // Throw a new error with the error message
+    throw new Error(errorMsg);
+  } else {
+    return await response.json();
+  }
+};
+
 export const fetchAmenities = async (): Promise<string[]> => {
   // Retrieve username and password from environment variables
   const username = process.env.NEXT_PUBLIC_API_USERNAME;
