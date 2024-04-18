bcba6504b51aa95bd2fa9b4fc09ca322b4cab9ce
Vincent Cai <72774400+vcai122@users.noreply.github.com>
2024-04-18 00:20:29 -0400

    Portal Admin Prettify (#283)
    * make it pretty
    
    * small typos
    
    * add link to message
    
    * i keep making silly mistake
    
    * fix more dum dum mistake
    
    * more recently created on top

---
+++ backend/pennmobile/admin.py
@@ -0,0 +1,34 @@
+# CUSTOM ADMIN SETTUP FOR PENN MOBILE
+from django.contrib import admin, messages
+from django.contrib.admin.apps import AdminConfig
+from django.urls import reverse
+from django.utils.html import format_html
+
+
+def add_post_poll_message(request, model):
+    if (count := model.objects.filter(model.ACTION_REQUIRED_CONDITION).count()) > 0:
+        link = reverse(f"admin:{model._meta.app_label}_{model._meta.model_name}_changelist")
+        messages.info(
+            request,
+            format_html(
+                f"Action Required: There {'is' if count == 1 else 'are'} {count} <a href='{link}'>"
+                + f"{model._meta.verbose_name if count == 1 else model._meta.verbose_name_plural}"
+                + "</a> that need to be reviewed."
+            ),
+        )
+
+
+class CustomAdminSite(admin.AdminSite):
+    site_header = "Penn Mobile Backend Admin"
+
+    def index(self, request, extra_context=None):
+        from portal.models import Poll, Post
+
+        add_post_poll_message(request, Post)
+        add_post_poll_message(request, Poll)
+
+        return super().index(request, extra_context)
+
+
+class PennMobileAdminConfig(AdminConfig):
+    default_site = "pennmobile.admin.CustomAdminSite"
--- backend/portal/admin.py
+++ backend/portal/admin.py
@@ -1,19 +1,85 @@
 from django.contrib import admin
 from django.utils.html import escape, mark_safe
 
-from portal.models import Poll, PollOption, PollVote, Post, TargetPopulation
+from portal.models import Content, Poll, PollOption, PollVote, Post, TargetPopulation
 
 
-class PostAdmin(admin.ModelAdmin):
-    def image_tag(self, instance):
-        return mark_safe('<img src="%s" height="300" />' % escape(instance.image.url))
+class ContentAdmin(admin.ModelAdmin):
+    @admin.action(description="Set status to Approved")
+    def action_approved(modeladmin, request, queryset):
+        queryset.update(status=Content.STATUS_APPROVED)
 
-    image_tag.short_description = "Post Image"
-    readonly_fields = ("image_tag",)
+    @admin.action(description="Set status to Draft")
+    def action_draft(modeladmin, request, queryset):
+        queryset.update(status=Content.STATUS_DRAFT)
+
+    @admin.action(description="Set status to Revision")
+    def action_revision(modeladmin, request, queryset):
+        queryset.update(status=Content.STATUS_REVISION)
+
+    actions = [action_approved, action_draft, action_revision]
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.annotate(ar=Content.ACTION_REQUIRED_CONDITION).order_by(
+            "-ar", "-created_date"
+        )
+
+    def ar(self, obj):
+        return obj.ar
+
+    ar.boolean = True
+
+
+class PostAdmin(ContentAdmin):
+    def image_tag(instance, height):
+        return mark_safe(
+            f'<img src="%s" height="{height}" />' % escape(instance.image and instance.image.url)
+        )
+
+    def small_image(self, instance):
+        return PostAdmin.image_tag(instance, 100)
+
+    small_image.short_description = "Post Image"
+
+    def large_image(self, instance):
+        return PostAdmin.image_tag(instance, 300)
+
+    large_image.short_description = "Post Image"
+
+    readonly_fields = ("large_image",)
+    list_display = (
+        "title",
+        "subtitle",
+        "club_code",
+        "club_comment",
+        "start_date",
+        "expire_date",
+        "ar",
+        "status",
+        "small_image",
+    )
+
+
+class PollOptionInline(admin.TabularInline):
+    model = PollOption
+    extra = 0
+
+
+class PollAdmin(ContentAdmin):
+    inlines = [PollOptionInline]
+    list_display = (
+        "question",
+        "club_code",
+        "club_comment",
+        "start_date",
+        "expire_date",
+        "ar",
+        "status",
+    )
 
 
 admin.site.register(TargetPopulation)
-admin.site.register(Poll)
-admin.site.register(PollOption)
+admin.site.register(Poll, PollAdmin)
 admin.site.register(PollVote)
 admin.site.register(Post, PostAdmin)
