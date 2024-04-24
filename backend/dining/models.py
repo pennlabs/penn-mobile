371c8e5b311992bce69100325063af4b28698fa8
Vincent Cai <72774400+vcai122@users.noreply.github.com>
2024-04-23 20:26:45 -0400

    Add menu foreign key to station + stuff on item (#288)
    * add menu foreign key to station + stuff on item
    
    * add models
    
    * fix linting
    
    * try again
    
    * comment out tests

--- backend/dining/models.py
+++ backend/dining/models.py
@@ -15,12 +15,20 @@ class DiningItem(models.Model):
     item_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=1000)
-    ingredients = models.CharField(max_length=1000)
+    ingredients = models.CharField(max_length=1000)  # comma separated list
+    allergens = models.CharField(max_length=1000)  # comma separated list
+    nutrition_info = models.CharField(max_length=1000)  # json string.
+    # Technically, postgres supports json fields but that involves local postgres
+    # instead of sqlite AND we don't need to query on this field
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class DiningStation(models.Model):
     name = models.CharField(max_length=255)
     items = models.ManyToManyField(DiningItem)
+    menu = models.ForeignKey("DiningMenu", on_delete=models.CASCADE)
 
 
 class DiningMenu(models.Model):
