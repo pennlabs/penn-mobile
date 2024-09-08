c24cdc2b8b984e41c4b89a586f141d23c5099366
Vincent Cai <72774400+vcai122@users.noreply.github.com>
2024-09-08 13:32:15 -0400

    Jsonify Nutrition Info (#304)

--- backend/dining/serializers.py
+++ backend/dining/serializers.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework import serializers
 
 from dining.models import DiningItem, DiningMenu, DiningStation, Venue
@@ -10,9 +12,17 @@ class VenueSerializer(serializers.ModelSerializer):
 
 
 class DiningItemSerializer(serializers.ModelSerializer):
+    nutrition_info = serializers.SerializerMethodField()
+
     class Meta:
         model = DiningItem
         fields = "__all__"
+
+    def get_nutrition_info(self, obj):
+        try:
+            return json.loads(obj.nutrition_info)
+        except json.JSONDecodeError:
+            return obj.nutrition_info
 
 
 class DiningStationSerializer(serializers.ModelSerializer):
