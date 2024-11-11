2d60616e17d57e17dba3ee910475d67bc518acb6
vcai122 <vincent.delaware@gmail.com>
2024-11-10 23:01:13 -0500

    Remove Me Groups

--- backend/user/models.py
+++ backend/user/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from gsr_booking.models import Group
 from laundry.models import LaundryRoom
 from penndata.models import FitnessRoom
 
@@ -71,7 +70,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     object exists for that User, it will create one
     """
     Profile.objects.get_or_create(user=instance)
-    Group.objects.get_or_create(owner=instance, name="Me", color="#14f7d1")
 
     # notifications
     token, _ = NotificationToken.objects.get_or_create(user=instance)
