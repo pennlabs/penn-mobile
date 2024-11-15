946e5f5f49c0628dc5fa5160d4f7b3ef8fbe7f4b
Jesse Zong <jessej.zong@gmail.com>
2024-11-15 18:26:29 -0500

    Lint

--- backend/tests/laundry/test_api_wrapper.py
+++ backend/tests/laundry/test_api_wrapper.py
@@ -1,128 +1,130 @@
-# from unittest import mock
-#
-# from django.conf import settings
-# from django.test import TestCase
-#
-# from laundry.api_wrapper import all_status, room_status, save_data
-# from laundry.models import LaundryRoom, LaundrySnapshot
-# from tests.laundry.test_commands import mock_laundry_get
-#
-#
-# ALL_URL = f"{settings.LAUNDRY_URL}/?location="
-#
-#
-# @mock.patch("requests.get", mock_laundry_get)
-# class TestAllStatus(TestCase):
-#     def setUp(self):
-#         LaundryRoom.objects.get_or_create(
-#             hall_id=0, name="Bishop White", location="Quad", total_washers=9, total_dryers=9
-#         )
-#         LaundryRoom.objects.get_or_create(
-#             hall_id=1, name="Chestnut Butcher", location="Quad", total_washers=11, total_dryers=11
-#         )
-#         LaundryRoom.objects.get_or_create(
-#             hall_id=2, name="Class of 1928 Fisher", location="Quad", total_washers=8, total_dryers=8
-#         )
-#         LaundryRoom.objects.get_or_create(
-#             hall_id=3, name="Craig", location="Quad", total_washers=3, total_dryers=3
-#         )
-#
-#     def test_all_status(self):
-#
-#         data = all_status()
-#
-#         self.assertEqual(len(data), 4)
-#
-#         for room in LaundryRoom.objects.all():
-#             self.assertIn(room.name, data)
-#
-#         # asserts fields are present and are positive numbers
-#         for hall_name, hall in data.items():
-#             for machine in ["washers", "dryers"]:
-#                 data = hall[machine]
-#
-#                 self.assertIn("running", data)
-#                 self.assertTrue(hall[machine]["running"] >= 0)
-#
-#                 self.assertIn("open", data)
-#                 self.assertTrue(hall[machine]["open"] >= 0)
-#
-#                 self.assertIn("out_of_order", data)
-#                 self.assertTrue(hall[machine]["out_of_order"] >= 0)
-#
-#                 self.assertIn("offline", data)
-#                 self.assertTrue(hall[machine]["offline"] >= 0)
-#
-#
-# @mock.patch("requests.get", mock_laundry_get)
-# class TestHallStatus(TestCase):
-#     def setUp(self):
-#         LaundryRoom.objects.get_or_create(
-#             hall_id=0, name="Bishop White", location="Quad", total_washers=9, total_dryers=9
-#         )
-#         LaundryRoom.objects.get_or_create(
-#             hall_id=1, name="Chestnut Butcher", location="Quad", total_washers=11, total_dryers=11
-#         )
-#         LaundryRoom.objects.get_or_create(
-#             hall_id=2, name="Class of 1928 Fisher", location="Quad", total_washers=8, total_dryers=8
-#         )
-#         LaundryRoom.objects.get_or_create(
-#             hall_id=3, name="Craig", location="Quad", total_washers=3, total_dryers=3
-#         )
-#
-#     def test_all_status(self):
-#
-#         for room in LaundryRoom.objects.all():
-#
-#             # asserts fields are present
-#             status = room_status(room)
-#             machines = status["machines"]
-#             self.assertIn("washers", machines)
-#             self.assertIn("dryers", machines)
-#
-#             for machine in machines["details"]:
-#                 self.assertIn("id", machine)
-#                 self.assertIn("type", machine)
-#                 self.assertIn("status", machine)
-#
-#
-# @mock.patch("requests.get", mock_laundry_get)
-# class TestSaveData(TestCase):
-#     def setUp(self):
-#         LaundryRoom.objects.get_or_create(
-#             hall_id=0, name="Bishop White", location="Quad", total_washers=9, total_dryers=9
-#         )
-#         LaundryRoom.objects.get_or_create(
-#             hall_id=1, name="Chestnut Butcher", location="Quad", total_washers=11, total_dryers=11
-#         )
-#         LaundryRoom.objects.get_or_create(
-#             hall_id=2, name="Class of 1928 Fisher", location="Quad", total_washers=8, total_dryers=8
-#         )
-#         LaundryRoom.objects.get_or_create(
-#             hall_id=3, name="Craig", location="Quad", total_washers=3, total_dryers=3
-#         )
-#
-#     def test_save_data(self):
-#
-#         self.assertEqual(LaundrySnapshot.objects.all().count(), 0)
-#
-#         save_data()
-#
-#         # checks that all rooms have been accounted for
-#         self.assertEqual(LaundrySnapshot.objects.all().count(), 4)
-#
-#         # # adds all id's to a list
-#         hall_ids = LaundryRoom.objects.all().values_list("hall_id", flat=True)
-#
-#         self.assertEqual(len(hall_ids), 4)
-#
-#         # asserts that fields are correct, and that all snapshots
-#         # have been accounted for
-#         for snapshot in LaundrySnapshot.objects.all():
-#             self.assertIn(snapshot.room.hall_id, hall_ids)
-#             self.assertTrue(snapshot.available_washers >= 0)
-#             self.assertTrue(snapshot.available_dryers >= 0)
-#
-#         save_data()
-#
-#         self.assertEqual(LaundrySnapshot.objects.all().count(), 8)
+'''
+from unittest import mock
+
+from django.conf import settings
+from django.test import TestCase
+
+from laundry.api_wrapper import all_status, room_status, save_data
+from laundry.models import LaundryRoom, LaundrySnapshot
+from tests.laundry.test_commands import mock_laundry_get
+
+
+ALL_URL = f"{settings.LAUNDRY_URL}/?location="
+
+
+@mock.patch("requests.get", mock_laundry_get)
+class TestAllStatus(TestCase):
+    def setUp(self):
+        LaundryRoom.objects.get_or_create(
+            hall_id=0, name="Bishop White", location="Quad", total_washers=9, total_dryers=9
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=1, name="Chestnut Butcher", location="Quad", total_washers=11, total_dryers=11
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=2, name="Class of 1928 Fisher", location="Quad", total_washers=8, total_dryers=8
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=3, name="Craig", location="Quad", total_washers=3, total_dryers=3
+        )
+
+    def test_all_status(self):
+
+        data = all_status()
+
+        self.assertEqual(len(data), 4)
+
+        for room in LaundryRoom.objects.all():
+            self.assertIn(room.name, data)
+
+        # asserts fields are present and are positive numbers
+        for hall_name, hall in data.items():
+            for machine in ["washers", "dryers"]:
+                data = hall[machine]
+
+                self.assertIn("running", data)
+                self.assertTrue(hall[machine]["running"] >= 0)
+
+                self.assertIn("open", data)
+                self.assertTrue(hall[machine]["open"] >= 0)
+
+                self.assertIn("out_of_order", data)
+                self.assertTrue(hall[machine]["out_of_order"] >= 0)
+
+                self.assertIn("offline", data)
+                self.assertTrue(hall[machine]["offline"] >= 0)
+
+
+@mock.patch("requests.get", mock_laundry_get)
+class TestHallStatus(TestCase):
+    def setUp(self):
+        LaundryRoom.objects.get_or_create(
+            hall_id=0, name="Bishop White", location="Quad", total_washers=9, total_dryers=9
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=1, name="Chestnut Butcher", location="Quad", total_washers=11, total_dryers=11
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=2, name="Class of 1928 Fisher", location="Quad", total_washers=8, total_dryers=8
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=3, name="Craig", location="Quad", total_washers=3, total_dryers=3
+        )
+
+    def test_all_status(self):
+
+        for room in LaundryRoom.objects.all():
+
+            # asserts fields are present
+            status = room_status(room)
+            machines = status["machines"]
+            self.assertIn("washers", machines)
+            self.assertIn("dryers", machines)
+
+            for machine in machines["details"]:
+                self.assertIn("id", machine)
+                self.assertIn("type", machine)
+                self.assertIn("status", machine)
+
+
+@mock.patch("requests.get", mock_laundry_get)
+class TestSaveData(TestCase):
+    def setUp(self):
+        LaundryRoom.objects.get_or_create(
+            hall_id=0, name="Bishop White", location="Quad", total_washers=9, total_dryers=9
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=1, name="Chestnut Butcher", location="Quad", total_washers=11, total_dryers=11
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=2, name="Class of 1928 Fisher", location="Quad", total_washers=8, total_dryers=8
+        )
+        LaundryRoom.objects.get_or_create(
+            hall_id=3, name="Craig", location="Quad", total_washers=3, total_dryers=3
+        )
+
+    def test_save_data(self):
+
+        self.assertEqual(LaundrySnapshot.objects.all().count(), 0)
+
+        save_data()
+
+        # checks that all rooms have been accounted for
+        self.assertEqual(LaundrySnapshot.objects.all().count(), 4)
+
+        # # adds all id's to a list
+        hall_ids = LaundryRoom.objects.all().values_list("hall_id", flat=True)
+
+        self.assertEqual(len(hall_ids), 4)
+
+        # asserts that fields are correct, and that all snapshots
+        # have been accounted for
+        for snapshot in LaundrySnapshot.objects.all():
+            self.assertIn(snapshot.room.hall_id, hall_ids)
+            self.assertTrue(snapshot.available_washers >= 0)
+            self.assertTrue(snapshot.available_dryers >= 0)
+
+        save_data()
+
+        self.assertEqual(LaundrySnapshot.objects.all().count(), 8)
+'''
--- backend/tests/laundry/test_commands.py
+++ backend/tests/laundry/test_commands.py
@@ -1,131 +1,133 @@
-# import csv
-# from io import StringIO
-# from unittest import mock
-#
-# from django.conf import settings
-# from django.core.management import call_command
-# from django.test import TestCase
-#
-# from laundry.models import LaundryRoom, LaundrySnapshot
-#
-#
-# def mock_laundry_get(url, *args, **kwargs):
-#     # TODO: should we do this with regex? using split bc I think it's cleaner
-#     split = url.split("/")
-#     if "/".join(split[0:3]) == settings.LAUNDRY_URL:
-#         if split[3] == "rooms":
-#             with open(f"tests/laundry/mock_rooms_request_{split[4]}.json", "rb") as f:
-#                 m = mock.MagicMock(content=f.read())
-#                 return m
-#         elif split[3] == "geoBoundaries":
-#             with open("tests/laundry/mock_geoboundaries_request.json", "rb") as f:
-#                 m = mock.MagicMock(content=f.read())
-#                 return m
-#         else:
-#             raise NotImplementedError
-#
-#
-# @mock.patch("requests.get", mock_laundry_get)
-# class TestGetSnapshot(TestCase):
-#     def setUp(self):
-#         # populates database with LaundryRooms
-#         LaundryRoom.objects.get_or_create(
-#             room_id=14089,
-#             name="English House",
-#             location="English House",
-#             location_id=14146,
-#             total_washers=3,
-#             total_dryers=3,
-#         )
-#
-#         LaundryRoom.objects.get_or_create(
-#             room_id=14099,
-#             name="Harnwell 10th Floor",
-#             location="Harnwell College House",
-#             location_id=14150,
-#             total_washers=3,
-#             total_dryers=3,
-#         )
-#
-#         LaundryRoom.objects.get_or_create(
-#             room_id=14100,
-#             name="Harnwell 12th Floor",
-#             location="Harnwell College House",
-#             location_id=14150,
-#             total_washers=3,
-#             total_dryers=3,
-#         )
-#
-#     def test_db_populate(self):
-#         out = StringIO()
-#         call_command("get_snapshot", stdout=out)
-#
-#         # tests the value of the output
-#         self.assertEqual("Captured snapshots!\n", out.getvalue())
-#
-#         # asserts that all rooms have been snapshotted
-#         self.assertEqual(LaundrySnapshot.objects.all().count(), 3)
-#
-#
-# @mock.patch("requests.get", mock_laundry_get)
-# class TestLaundryRoomMigration(TestCase):
-#     def test_db_populate(self):
-#         out = StringIO()
-#         call_command("load_laundry_rooms", stdout=out)
-#
-#         # tests the value of the output
-#         self.assertEqual("Uploaded Laundry Rooms!\n", out.getvalue())
-#
-#         # compute expected number of rooms
-#         with open("laundry/data/laundry_data.csv") as data:
-#             reader = csv.reader(data)
-#             # subtract 1 to account for header
-#             num_rooms = sum(1 for _ in reader) - 1
-#
-#         # asserts that the number of LaundryRooms created was 53
-#         self.assertEqual(LaundryRoom.objects.all().count(), num_rooms)
-#
-#         with open("laundry/data/laundry_data.csv") as data:
-#             reader = csv.DictReader(data)
-#
-#             for row in reader:
-#                 room = LaundryRoom.objects.get(room_id=int(row["room_id"]))
-#
-#                 # asserts that all fields of LaundryRoom are same
-#                 self.assertEqual(room.name, row["room_name"])
-#                 self.assertEqual(room.location, row["room_description"])
-#                 self.assertEqual(room.location_id, int(row["room_location"]))
-#                 self.assertEqual(room.total_washers, int(row["count_washers"]))
-#                 self.assertEqual(room.total_dryers, int(row["count_dryers"]))
-#         call_command("load_laundry_rooms")
-#
-#         # asserts that LaundryRooms do not recreate itself
-#
-#
-# @mock.patch("requests.get", mock_laundry_get)
-# class TestUpdateLaundryRooms(TestCase):
-#     @mock.patch("laundry.management.commands.update_laundry_rooms.write_file")
-#     def test_update_laundry_rooms(self, mock_laundry_write):
-#         call_command("update_laundry_rooms")
-#         expected = [
-#             {
-#                 "room_id": 14089,
-#                 "room_name": "English House",
-#                 "room_description": "English House",
-#                 "room_location": 14146,
-#             },
-#             {
-#                 "room_id": 14099,
-#                 "room_name": "Harnwell 10th Floor",
-#                 "room_description": "Harnwell College House",
-#                 "room_location": 14150,
-#             },
-#             {
-#                 "room_id": 14100,
-#                 "room_name": "Harnwell 12th Floor",
-#                 "room_description": "Harnwell College House",
-#                 "room_location": 14150,
-#             },
-#         ]
-#         # assert that the mock was called with this
-#         mock_laundry_write.assert_called_with(expected)
+'''
+import csv
+from io import StringIO
+from unittest import mock
+
+from django.conf import settings
+from django.core.management import call_command
+from django.test import TestCase
+
+from laundry.models import LaundryRoom, LaundrySnapshot
+
+
+def mock_laundry_get(url, *args, **kwargs):
+    # TODO: should we do this with regex? using split bc I think it's cleaner
+    split = url.split("/")
+    if "/".join(split[0:3]) == settings.LAUNDRY_URL:
+        if split[3] == "rooms":
+            with open(f"tests/laundry/mock_rooms_request_{split[4]}.json", "rb") as f:
+                m = mock.MagicMock(content=f.read())
+                return m
+        elif split[3] == "geoBoundaries":
+            with open("tests/laundry/mock_geoboundaries_request.json", "rb") as f:
+                m = mock.MagicMock(content=f.read())
+                return m
+        else:
+            raise NotImplementedError
+
+
+@mock.patch("requests.get", mock_laundry_get)
+class TestGetSnapshot(TestCase):
+    def setUp(self):
+        # populates database with LaundryRooms
+        LaundryRoom.objects.get_or_create(
+            room_id=14089,
+            name="English House",
+            location="English House",
+            location_id=14146,
+            total_washers=3,
+            total_dryers=3,
+        )
+
+        LaundryRoom.objects.get_or_create(
+            room_id=14099,
+            name="Harnwell 10th Floor",
+            location="Harnwell College House",
+            location_id=14150,
+            total_washers=3,
+            total_dryers=3,
+        )
+
+        LaundryRoom.objects.get_or_create(
+            room_id=14100,
+            name="Harnwell 12th Floor",
+            location="Harnwell College House",
+            location_id=14150,
+            total_washers=3,
+            total_dryers=3,
+        )
+
+    def test_db_populate(self):
+        out = StringIO()
+        call_command("get_snapshot", stdout=out)
+
+        # tests the value of the output
+        self.assertEqual("Captured snapshots!\n", out.getvalue())
+
+        # asserts that all rooms have been snapshotted
+        self.assertEqual(LaundrySnapshot.objects.all().count(), 3)
+
+
+@mock.patch("requests.get", mock_laundry_get)
+class TestLaundryRoomMigration(TestCase):
+    def test_db_populate(self):
+        out = StringIO()
+        call_command("load_laundry_rooms", stdout=out)
+
+        # tests the value of the output
+        self.assertEqual("Uploaded Laundry Rooms!\n", out.getvalue())
+
+        # compute expected number of rooms
+        with open("laundry/data/laundry_data.csv") as data:
+            reader = csv.reader(data)
+            # subtract 1 to account for header
+            num_rooms = sum(1 for _ in reader) - 1
+
+        # asserts that the number of LaundryRooms created was 53
+        self.assertEqual(LaundryRoom.objects.all().count(), num_rooms)
+
+        with open("laundry/data/laundry_data.csv") as data:
+            reader = csv.DictReader(data)
+
+            for row in reader:
+                room = LaundryRoom.objects.get(room_id=int(row["room_id"]))
+
+                # asserts that all fields of LaundryRoom are same
+                self.assertEqual(room.name, row["room_name"])
+                self.assertEqual(room.location, row["room_description"])
+                self.assertEqual(room.location_id, int(row["room_location"]))
+                self.assertEqual(room.total_washers, int(row["count_washers"]))
+                self.assertEqual(room.total_dryers, int(row["count_dryers"]))
+        call_command("load_laundry_rooms")
+
+        # asserts that LaundryRooms do not recreate itself
+
+
+@mock.patch("requests.get", mock_laundry_get)
+class TestUpdateLaundryRooms(TestCase):
+    @mock.patch("laundry.management.commands.update_laundry_rooms.write_file")
+    def test_update_laundry_rooms(self, mock_laundry_write):
+        call_command("update_laundry_rooms")
+        expected = [
+            {
+                "room_id": 14089,
+                "room_name": "English House",
+                "room_description": "English House",
+                "room_location": 14146,
+            },
+            {
+                "room_id": 14099,
+                "room_name": "Harnwell 10th Floor",
+                "room_description": "Harnwell College House",
+                "room_location": 14150,
+            },
+            {
+                "room_id": 14100,
+                "room_name": "Harnwell 12th Floor",
+                "room_description": "Harnwell College House",
+                "room_location": 14150,
+            },
+        ]
+        # assert that the mock was called with this
+        mock_laundry_write.assert_called_with(expected)
+'''
