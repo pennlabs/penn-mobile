1319fdb50b30d3411f49cb9dcb2e8b698c0555b2
vcai122 <vincent.delaware@gmail.com>
2024-11-19 01:26:08 -0500

    Add Urgent Notif (iOS)

--- backend/user/notifications.py
+++ backend/user/notifications.py
@@ -27,8 +27,8 @@ from celery import shared_task
 
 
 class NotificationWrapper(ABC):
-    def send_notification(self, tokens, title, body):
-        self.send_payload(tokens, self.create_payload(title, body))
+    def send_notification(self, tokens, title, body, urgent):
+        self.send_payload(tokens, self.create_payload(title, body, urgent))
 
     def send_shadow_notification(self, tokens, body):
         self.send_payload(tokens, self.create_shadow_payload(body))
@@ -42,7 +42,7 @@ class NotificationWrapper(ABC):
             self.send_one_notification(tokens[0], payload)
 
     @abstractmethod
-    def create_payload(self, title, body):
+    def create_payload(self, title, body, urgent):
         raise NotImplementedError  # pragma: no cover
 
     @abstractmethod
@@ -67,7 +67,8 @@ class AndroidNotificationWrapper(NotificationWrapper):
         except Exception as e:
             print(f"Notifications Error: Failed to initialize Firebase client: {e}")
 
-    def create_payload(self, title, body):
+    def create_payload(self, title, body, urgent):
+        # TODO: do something with urgent
         return {"notification": messaging.Notification(title=title, body=body)}
 
     def create_shadow_payload(self, body):
@@ -84,6 +85,17 @@ class AndroidNotificationWrapper(NotificationWrapper):
 
 
 class IOSNotificationWrapper(NotificationWrapper):
+    class CustomPayload(Payload):
+        # Custom payload to support interruption_level
+        def __init__(self, urgent, **kwargs):
+            super().__init__(**kwargs)
+            self.urgent = urgent
+
+        def dict(self):
+            result = super().dict()
+            if self.urgent:
+                result["aps"]["interruption-level"] = "time-sensitive"
+
     @staticmethod
     def get_client(is_dev):
         auth_key_path = (
@@ -98,10 +110,14 @@ class IOSNotificationWrapper(NotificationWrapper):
         except Exception as e:
             print(f"Notifications Error: Failed to initialize APNs client: {e}")
 
-    def create_payload(self, title, body):
+    def create_payload(self, title, body, urgent):
         # TODO: we might want to add category here, but there is no use on iOS side for now
-        return Payload(
-            alert={"title": title, "body": body}, sound="default", badge=0, mutable_content=True
+        return IOSNotificationWrapper.CustomPayload(
+            alert={"title": title, "body": body},
+            sound="default",
+            badge=0,
+            mutable_content=True,
+            urgent=urgent,
         )
 
     def create_shadow_payload(self, body):
@@ -121,7 +137,7 @@ IOSNotificationDevSender = IOSNotificationWrapper(is_dev=True)
 
 
 @shared_task(name="notifications.ios_send_notification")
-def ios_send_notification(tokens, title, body):
+def ios_send_notification(tokens, title, body, urgent):
     IOSNotificationSender.send_notification(tokens, title, body)
 
 
@@ -131,7 +147,7 @@ def ios_send_shadow_notification(tokens, body):
 
 
 @shared_task(name="notifications.android_send_notification")
-def android_send_notification(tokens, title, body):
+def android_send_notification(tokens, title, body, urgent):
     AndroidNotificationSender.send_notification(tokens, title, body)
 
 
@@ -141,7 +157,7 @@ def android_send_shadow_notification(tokens, body):
 
 
 @shared_task(name="notifications.ios_send_dev_notification")
-def ios_send_dev_notification(tokens, title, body):
+def ios_send_dev_notification(tokens, title, body, urgent):
     IOSNotificationDevSender.send_notification(tokens, title, body)
 
 
