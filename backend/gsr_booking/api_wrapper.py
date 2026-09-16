92581f2a63bbcd807ae11577465b3cfe3aff7bc1
Eunsoo Shin <62971511+esinx@users.noreply.github.com>
2026-09-15 22:02:34 -0400

    fix(is_wharton): return false if 403 (#433)
    * fix(is_wharton): return false if 403
    
    * fix(APIError): fix args
    
    * remove 504 from timeout, set 0 as default
    
    * remove message prop
    
    * refactor(APIError): Better typing for APIError

--- backend/gsr_booking/api_wrapper.py
+++ backend/gsr_booking/api_wrapper.py
@@ -78,7 +78,7 @@ class WhartonBookingWrapper(AbstractBookingWrapper):
             raise APIError("Wharton: Connection timeout")
 
         if not response.ok:
-            raise APIError(f"Wharton: Error {response.status_code} when reserving data")
+            raise APIError(f"Wharton: Error {response.status_code} when reserving data", response.status_code)
 
         return response
 
@@ -175,6 +175,9 @@ class WhartonBookingWrapper(AbstractBookingWrapper):
             res_json = response.json()
             return res_json.get("type") == "whartonMBA" or res_json.get("type") == "whartonUGR"
         except APIError as e:
+            if e.status_code == 403:
+                # default to False if API returns 403
+                return False
             logger.error(f"Wharton API error for {user.username}: {e}")
             return False
 
--- backend/utils/errors.py
+++ backend/utils/errors.py
@@ -1,2 +1,4 @@
 class APIError(ValueError):
-    pass
+    def __init__(self, message: str, status_code: int | None = None):
+        self.status_code = status_code
+        super().__init__(message)
