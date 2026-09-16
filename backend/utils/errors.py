051c7961558ba64ec1d85adaf62a171f00b7a7f0
Eunsoo Shin <me@esinx.net>
2026-09-15 21:59:54 -0400

    remove message prop

--- backend/utils/errors.py
+++ backend/utils/errors.py
@@ -1,5 +1,4 @@
 class APIError(ValueError):
     def __init__(self, message: str, status_code=0):
         self.status_code = status_code
-        self.message = message
         super().__init__(message)
