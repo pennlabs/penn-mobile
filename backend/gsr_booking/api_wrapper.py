fc6377a6e4e94bc5835da132c43e72bbb0ec8039
Eunsoo Shin <me@esinx.net>
2026-09-15 21:58:22 -0400

    remove 504 from timeout, set 0 as default

--- backend/gsr_booking/api_wrapper.py
+++ backend/gsr_booking/api_wrapper.py
@@ -74,8 +74,8 @@ class WhartonBookingWrapper(AbstractBookingWrapper):
         try:
             response = requests.request(*args, **kwargs)
         except (ConnectTimeout, ReadTimeout, ConnectionError):
-            # technically wrong, but 504 is reasonable for timeout
-            raise APIError("Wharton: Connection timeout", 504)
+            raise APIError("Wharton: Connection timeout")
+
         if not response.ok:
             raise APIError(
                 f"Wharton: Error {response.status_code} when reserving data",
