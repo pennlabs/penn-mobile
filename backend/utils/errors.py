94d13880e89011361d76b3d0320e6360ed139d17
Eunsoo Shin <me@esinx.net>
2026-09-15 21:57:13 -0400

    fix(APIError): fix args

--- backend/utils/errors.py
+++ backend/utils/errors.py
@@ -1,5 +1,5 @@
 class APIError(ValueError):
-    def __init__(self, status_code, message):
+    def __init__(self, message: str, status_code=0):
         self.status_code = status_code
         self.message = message
         super().__init__(message)
